47cb27b77aaded2d5819045c8b504669a24b3f82
Kimiyuki Onaka <kimiyuki95@gmail.com>
2019-07-19 23:29:54 +0900

    update transpiler

--- Transpile.fs
+++ Transpile.fs
@@ -30,115 +30,122 @@ let getCXXTypeFromSchema : Schema<RType> -> string =
     | Polytype _ as scm -> failwithf "failed to convert to a C++ type: %A" scm
     | Monotype t -> getCXXType t
 
-let indent (nest : int) : string = String.replicate nest "    "
-
 let join (sep : string) (words : list<string>) : string =
     List.fold (fun acc (i, word) ->
         acc + (if i = 0 then ""
                else sep)
         + word) "" (List.indexed words)
 
-let transpileExpr (gensym : unit -> string) (sb : StringBuilder) : int -> Expr -> StringBuilder =
-    let appendLine (nest : int) (s : string) =
-        let _ = sb.Append(indent nest)
-        let _ = sb.Append s
+type CXXCode =
+    { sentences : list<string>
+      expr : string }
 
-        let _ =
-            sb.Append "\n"
-        ()
+let cxxExpr (s : string) : CXXCode =
+    { sentences = []
+      expr = s }
 
-    let rec go (env : list<string>) (nest : int) : Expr -> unit =
+let bindCXXCode (code : CXXCode) (sentences : list<string>) (expr : string -> string) =
+    { sentences = List.append sentences code.sentences
+      expr = expr code.expr }
+
+let transpileExpr (gensym_counter : unit -> string) (gensym_accumulator : unit -> string) (gensym_general : unit -> string) : Expr -> CXXCode =
+    let rec go (env : list<string>) : Expr -> CXXCode =
         let accumulate plus zero n e =
-            let acc = gensym()
-            let cnt = gensym()
-            appendLine nest (sprintf "int64_t %s = %s;" acc zero)
-            appendLine nest (sprintf "int64_t %s = ([&]() {" cnt)
-            go env (nest + 1) n
-            appendLine nest "})();"
-            appendLine nest (sprintf "while (%s --) {" cnt)
-            let t = gensym()
-            appendLine (nest + 1) (sprintf "int64_t %s = ([&]() {" t)
-            go (cnt :: env) (nest + 2) e
-            appendLine (nest + 1) "})();"
-            appendLine (nest + 1) (sprintf "%s = %s;" acc (plus acc t))
-            appendLine nest "}"
-            appendLine nest (sprintf "return %s;" acc)
-
-        let cast t e =
-            appendLine nest (sprintf "return ([&]() -> %s {" (getCXXType t))
-            go env (nest + 1) e
-            appendLine nest "})();"
-
+            let sentences = ref []
+            let line s = sentences := s :: !sentences
+            let acc = gensym_accumulator()
+            line (sprintf "int64_t %s = %s;" acc zero)
+            let n = go env n
+            sentences := List.append n.sentences !sentences
+            let i = gensym_counter()
+            line (sprintf "for (int64_t %s = 0; %s < %s; ++ %s) {" i i n.expr i)
+            let e = go (i :: env) e
+            sentences := List.append e.sentences !sentences
+            line (plus acc e.expr + ";")
+            line "}"
+            { sentences = !sentences
+              expr = acc }
         function
-        | AppExp(AppExp(FreeVarExp(ValName "count", _), n), LamExp(_, e)) -> accumulate (sprintf "%s + (bool)(%s)") "0" n e
-        | AppExp(AppExp(FreeVarExp(ValName "sum", _), n), LamExp(_, e)) -> accumulate (sprintf "%s + %s") "0" n e
-        | AppExp(AppExp(FreeVarExp(ValName "max", _), n), LamExp(_, e)) -> accumulate (sprintf "max(%s, %s)") "INT64_MIN" n e
-        | AppExp(AppExp(FreeVarExp(ValName "min", _), n), LamExp(_, e)) -> accumulate (sprintf "min(%s, %s)") "INT64_MAX" n e
-        | AppExp(FreeVarExp(ValName "zahlToBool", _), e) -> cast BoolRTy e
-        | AppExp(FreeVarExp(ValName "boolToZahl", _), e) -> cast ZahlRTy e
-        | VarExp i -> appendLine nest (sprintf "return %s;" env.[i])
-        | FreeVarExp(ValName x, _) -> appendLine nest (sprintf "return %s;" x)
+        | AppExp(AppExp(FreeVarExp(ValName "count", _), n), LamExp(_, e)) -> accumulate (sprintf "%s += (bool)(%s)") "0" n e
+        | AppExp(AppExp(FreeVarExp(ValName "sum", _), n), LamExp(_, e)) -> accumulate (sprintf "%s += %s") "0" n e
+        | AppExp(AppExp(FreeVarExp(ValName "max", _), n), LamExp(_, e)) -> accumulate (fun a b -> sprintf "%s = max(%s, %s)" a a b) "INT64_MIN" n e
+        | AppExp(AppExp(FreeVarExp(ValName "min", _), n), LamExp(_, e)) -> accumulate (fun a b -> sprintf "%s = min(%s, %s)" a a b) "INT64_MAX" n e
+        | AppExp(FreeVarExp(ValName "zahlToBool", _), e) -> bindCXXCode (go env e) [] (sprintf "(bool)(%s)")
+        | AppExp(FreeVarExp(ValName "boolToZahl", _), e) -> bindCXXCode (go env e) [] (sprintf "(int64_t)(%s)")
+        | VarExp i -> cxxExpr env.[i]
+        | FreeVarExp(ValName x, _) -> cxxExpr x
         | LamExp _ as e -> failwithf "failed to transpile: %A" e
         | AppExp _ as e ->
-            let rec loop args =
+            let rec loop args sentences =
                 function
                 | AppExp(e1, e2) ->
-                    let t = gensym()
-                    appendLine nest (sprintf "auto %s = ([&]() {" t)
-                    go env (nest + 1) e2
-                    appendLine nest "})();"
-                    loop (t :: args) e1
+                    let e2 = go env e2
+                    loop (e2.expr :: args) (List.append sentences e2.sentences) e1
                 | FreeVarExp(ValName name, _) ->
                     let s =
                         match name with
                         | _ when name = "+" || name = "-" || name = "*" || name = "/" || name = "%" -> join (" " + name + " ") args
                         | _ when name = "**" -> sprintf "pow(%s)" (join ", " args)
                         | _ -> sprintf "%s[%s]" name (join "][" args)
-                    appendLine nest (sprintf "return %s;" s)
+                    { sentences = sentences
+                      expr = s }
                 | _ -> failwith "failed to transpile"
-            loop [] e
+            loop [] [] e
         | IfThenElseExp(e1, e2, e3) ->
-            let t = gensym()
-            appendLine nest (sprintf "bool %s = ([&]() {" t)
-            appendLine nest "})();"
-            appendLine nest (sprintf "if (%s) {" t)
-            go env (nest + 1) e2
-            appendLine nest (sprintf "} else {")
-            go env (nest + 1) e3
-            appendLine nest (sprintf "}")
-        | IntExp n -> appendLine nest (sprintf "return %A;" n)
+            let e1 = go env e1
+            let e2 = go env e2
+            let e3 = go env e3
+            if e2.sentences = [] && e3.sentences = [] then bindCXXCode e1 [] (fun e1 -> sprintf "(%s ? %s : %s)" e1 e2.expr e3.expr)
+            else
+                let sentences = ref e1.sentences
+                let line s = sentences := s :: !sentences
+                let t = gensym_general()
+                line (sprintf "auto %s = ([&]() {" t)
+                line (sprintf "if (%s) {" e1.expr)
+                sentences := List.append e2.sentences !sentences
+                line (sprintf "return %s;" e2.expr)
+                line (sprintf "} else {")
+                sentences := List.append e3.sentences !sentences
+                line (sprintf "return %s;" e3.expr)
+                line (sprintf "}")
+                { sentences = !sentences
+                  expr = t }
+        | IntExp n -> cxxExpr (sprintf "%A" n)
         | BoolExp p ->
-            appendLine nest (sprintf "return %s;" (if p then "true"
-                                                   else "false"))
+            cxxExpr (if p then "true"
+                     else "false")
+    go []
 
-    fun nest e ->
-        go [] nest e
-        sb
+let indent (nest : int) : string = String.replicate nest "    "
 
-let transpileDefinition (gensym : unit -> string) (sb : StringBuilder) (nest : int) (name : ValName) (e : Expr) (scm : Schema<RType>) : StringBuilder =
+let appendLine (sb : StringBuilder) (nest : int) (s : string) : unit =
     let _ = sb.Append(indent nest)
-    let _ = sb.Append(getCXXTypeFromSchema scm)
-    let _ = sb.Append " "
-    match name with
-    | ValName name ->
-        let _ = sb.Append name
-        ()
-    let _ =
-        sb.Append " = ([&]() {\n"
-    match scm with
-    | Polytype _ as scm -> failwithf "failed to transpile: %A" scm
-    | Monotype t ->
-        let _ = transpileExpr gensym sb (nest + 1) e
-        ()
-    let _ = sb.Append(indent nest)
+    let _ = sb.Append s
 
     let _ =
-        sb.Append "})();\n"
-    sb
+        sb.Append "\n"
+    ()
+
+let appendCXXCode (sb : StringBuilder) (nest : int) (code : CXXCode) (cont : string -> string) : unit =
+    let rec go (nest : int) : list<string> -> unit =
+        function
+        | [] -> appendLine sb nest (cont code.expr)
+        | sentence :: sentences ->
+            let nest =
+                (if sentence.StartsWith "}" then nest - 1
+                 else nest)
+            appendLine sb nest sentence
+            let nest =
+                (if sentence.EndsWith "{" then nest + 1
+                 else nest)
+            go nest sentences
+    go nest (List.rev code.sentences)
 
 let transpile (given : list<ValName * RType>) (definition : list<ValName * Expr * Schema<RType>>) (e : Expr) (scm : Schema<RType>) : string =
     let sb = new StringBuilder()
-    let gensym_cxx = newGensym id "t"
+    let gensym_accumulator = newGensym id "a"
+    let gensym_counter = newGensym id "i"
+    let gensym_general = newGensym id "t"
     let _ = sb.Append(getCXXTypeFromSchema scm)
     let _ = sb.Append " solve("
     for (i, (ValName x, t)) in List.indexed (List.rev given) do
@@ -152,11 +159,9 @@ let transpile (given : list<ValName * RType>) (definition : list<ValName * Expr 
         ()
     let _ =
         sb.Append ") {\n"
-    for (x, e, scm) in List.rev definition do
-        let _ = transpileDefinition gensym_cxx sb 1 x e scm
-        ()
-    let _ = transpileExpr gensym_cxx sb 1 e
-
+    for (ValName x, e, scm) in List.rev definition do
+        appendCXXCode sb 1 (transpileExpr gensym_counter gensym_accumulator gensym_general e) (fun e -> sprintf "%s %s = %s;" (getCXXTypeFromSchema scm) x e)
+    appendCXXCode sb 1 (transpileExpr gensym_counter gensym_accumulator gensym_general e) (sprintf "return %s;")
     let _ =
         sb.Append "}\n"
     sb.ToString()
