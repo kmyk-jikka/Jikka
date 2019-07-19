996d97a5a7581b2f5152b574a06cd2b818c4d9fb
Kimiyuki Onaka <kimiyuki95@gmail.com>
2019-07-19 22:32:12 +0900

    update optimizer

--- Optimization.fs
+++ Optimization.fs
@@ -38,10 +38,10 @@ let foldBoundVar (f : 'a -> int -> 'a) : 'a -> Expr -> 'a =
         | BoolExp _ -> acc
     go 0
 
-let mapBoundVar (f : int -> Expr) : Expr -> Expr =
+let mapBoundVar (f : int -> int -> Expr) : Expr -> Expr =
     let rec go depth e =
         match e with
-        | VarExp i -> f (i - depth)
+        | VarExp i -> f depth i
         | FreeVarExp _ -> e
         | LamExp(t, e1) -> LamExp(t, go (depth + 1) e1)
         | AppExp(e1, e2) -> AppExp(go depth e1, go depth e2)
@@ -53,14 +53,14 @@ let mapBoundVar (f : int -> Expr) : Expr -> Expr =
 let findBoundVar (n : int) : Expr -> bool = foldBoundVar (fun acc i -> acc || i = n) false
 
 let unshiftLambda : Expr -> Expr =
-    mapBoundVar (fun i ->
-        VarExp(if i > 0 then i - 1
-               elif i < 0 then i
+    mapBoundVar (fun depth i ->
+        VarExp(if i > depth then i - 1
+               elif i < depth then i
                else failwith "failed to shift"))
 
 let assignLambda (n : int) (e : Expr) : Expr -> Expr =
-    mapBoundVar (fun i ->
-        if i = n then e
+    mapBoundVar (fun depth i ->
+        if i = n + depth then e
         else VarExp i)
 
 let optimize (gensym : unit -> TyName) : Expr -> Expr =
@@ -73,6 +73,7 @@ let optimize (gensym : unit -> TyName) : Expr -> Expr =
         | Some(x, scm) -> FreeVarExp(x, realizeSchema gensym scm)
 
     let appf name x = AppExp(fvar name, x)
+    let app2f name x y = AppExp(AppExp(fvar name, x), y)
     let add : Expr -> Expr -> Expr = app2 (fvar "+")
     let sub : Expr -> Expr -> Expr = app2 (fvar "-")
     let mul : Expr -> Expr -> Expr = app2 (fvar "*")
@@ -88,67 +89,97 @@ let optimize (gensym : unit -> TyName) : Expr -> Expr =
             let (f, t1) = go acc f
             let (x, t2) = go acc x
 
+            let go e =
+                let (e, _) = go acc e in e
+
+            let nop = AppExp(f, x)
+
             let y =
-                let go e =
-                    let (e, _) = go acc e in e
-                match (f, x) with
-                | (AppExp(FreeVarExp(ValName "add", _), IntExp a), IntExp b) -> IntExp(a + b)
-                | (AppExp(FreeVarExp(ValName "sub", _), IntExp a), IntExp b) -> IntExp(a - b)
-                | (AppExp(FreeVarExp(ValName "mul", _), IntExp a), IntExp b) -> IntExp(a * b)
-                | (AppExp(FreeVarExp(ValName "div", _), IntExp a), IntExp b) -> IntExp(a / b)
-                | (AppExp(FreeVarExp(ValName "mod", _), IntExp a), IntExp b) -> IntExp(a % b)
-                | (AppExp(FreeVarExp(ValName "pow", _), IntExp a), IntExp b) -> IntExp(a ** int (b))
-                | (AppExp(FreeVarExp(ValName "add", _), e), IntExp n) when n = 0I -> e
-                | (AppExp(FreeVarExp(ValName "sub", _), e), IntExp n) when n = 0I -> e
-                | (AppExp(FreeVarExp(ValName "mul", _), e), IntExp n) when n = 1I -> e
-                | (AppExp(FreeVarExp(ValName "mul", _), _), IntExp n) when n = 0I -> IntExp 0I
-                | (AppExp(FreeVarExp(ValName "mul", _), e), IntExp n) when n = -1I -> go (app (fvar "negate") e)
-                | (AppExp(FreeVarExp(ValName "div", _), e), IntExp n) when n = 1I -> e
-                | (AppExp(FreeVarExp(ValName "div", _), e), IntExp n) when n = -1I -> go (app (fvar "negate") e)
-                | (AppExp(FreeVarExp(ValName "mod", _), _), IntExp n) when n = 1I -> IntExp 0I
-                | (AppExp(FreeVarExp(ValName "pow", _), e), IntExp n) when n = 1I -> e
-                | (AppExp(FreeVarExp(ValName "add", _), IntExp n), e) when n = 0I -> e
-                | (AppExp(FreeVarExp(ValName "sub", _), IntExp n), e) when n = 0I -> go (app (fvar "negate") e)
-                | (AppExp(FreeVarExp(ValName "mul", _), IntExp n), e) when n = 1I -> e
-                | (AppExp(FreeVarExp(ValName "mul", _), IntExp n), _) when n = 0I -> IntExp 0I
-                | (AppExp(FreeVarExp(ValName "mul", _), IntExp n), e) when n = -1I -> go (app (fvar "negate") e)
-                | (AppExp(FreeVarExp(ValName "div", _), IntExp n), _) when n = 0I -> IntExp 0I
-                | (AppExp(FreeVarExp(ValName "div", _), IntExp n), e) when n = -1I -> go (app (fvar "negate") e)
-                | (AppExp(FreeVarExp(ValName "mod", _), IntExp n), _) when n = 0I -> IntExp 0I
-                | (AppExp(FreeVarExp(ValName "pow", _), IntExp n), _) when n = 1I -> IntExp 1I
-                | (FreeVarExp(ValName "negate", _), IntExp n) -> IntExp(-n)
-                | (FreeVarExp(ValName "negate", _), AppExp(FreeVarExp(ValName "negate", _), e)) -> e
-                | (FreeVarExp(ValName "!", _), BoolExp p) -> BoolExp(not p)
-                | (FreeVarExp(ValName "!", _), AppExp(FreeVarExp(ValName "!", _), e)) -> e
-                | (AppExp(FreeVarExp(ValName "&&", _), BoolExp a), BoolExp b) -> BoolExp(a && b)
-                | (AppExp(FreeVarExp(ValName "||", _), BoolExp a), BoolExp b) -> BoolExp(a || b)
-                | (AppExp(FreeVarExp(ValName "&&", _), BoolExp true), e) -> e
-                | (AppExp(FreeVarExp(ValName "&&", _), BoolExp false), _) -> BoolExp false
-                | (AppExp(FreeVarExp(ValName "||", _), BoolExp true), _) -> BoolExp true
-                | (AppExp(FreeVarExp(ValName "||", _), BoolExp false), e) -> e
-                | (AppExp(FreeVarExp(ValName "&&", _), e), BoolExp true) -> e
-                | (AppExp(FreeVarExp(ValName "&&", _), _), BoolExp false) -> BoolExp false
-                | (AppExp(FreeVarExp(ValName "||", _), _), BoolExp true) -> BoolExp true
-                | (AppExp(FreeVarExp(ValName "||", _), e), BoolExp false) -> e
-                | (AppExp(FreeVarExp(ValName "<", _), IntExp a), IntExp b) -> BoolExp(a < b)
-                | (AppExp(FreeVarExp(ValName "<=", _), IntExp a), IntExp b) -> BoolExp(a <= b)
-                | (AppExp(FreeVarExp(ValName "=", _), IntExp a), IntExp b) -> BoolExp(a = b)
-                | (AppExp(FreeVarExp(ValName "<>", _), IntExp a), IntExp b) -> BoolExp(a <> b)
-                | (AppExp(FreeVarExp(ValName ">=", _), IntExp a), IntExp b) -> BoolExp(a >= b)
-                | (AppExp(FreeVarExp(ValName ">", _), IntExp a), IntExp b) -> BoolExp(a > b)
-                | (AppExp(FreeVarExp(ValName "=", _), e), IntExp n) when n = 0I -> go (appf "!" (appf "zahlToBool" e))
-                | (AppExp(FreeVarExp(ValName "=", _), IntExp n), e) when n = 0I -> go (appf "!" (appf "zahlToBool" e))
-                | (AppExp(FreeVarExp(ValName "<>", _), e), IntExp n) when n = 0I -> go (appf "zahlToBool" e)
-                | (AppExp(FreeVarExp(ValName "<>", _), IntExp n), e) when n = 0I -> go (appf "zahlToBool" e)
-                | (AppExp(FreeVarExp(ValName "count", _), IntExp n), _) when n = 0I -> IntExp 0I
-                | (AppExp(FreeVarExp(ValName "count", _), n), LamExp(_, e)) when not (findBoundVar 0 e) -> go (mul n (appf "boolToZahl" (unshiftLambda e)))
-                | (AppExp(FreeVarExp(ValName "sum", _), IntExp n), _) when n = 0I -> IntExp 0I
-                | (AppExp(FreeVarExp(ValName "sum", _), n), LamExp(_, e)) when not (findBoundVar 0 e) -> go (mul n (unshiftLambda e))
-                | (AppExp(FreeVarExp(ValName "sum", _), n), LamExp(t, AppExp(AppExp(FreeVarExp(ValName "+", _), e1), e2))) ->
-                    let e1 = app2 (fvar "sum") n (LamExp(t, e1))
-                    let e2 = app2 (fvar "sum") n (LamExp(t, e2))
-                    go (add e1 e2)
-                | _ -> AppExp(f, x)
+                match AppExp(f, x) with
+                | AppExp(FreeVarExp(ValName f, _), x) ->
+                    match (f, x) with
+                    | ("negate", IntExp n) -> IntExp(-n)
+                    | ("negate", AppExp(FreeVarExp(ValName "negate", _), e)) -> e
+                    | ("!", BoolExp p) -> BoolExp(not p)
+                    | ("!", AppExp(FreeVarExp(ValName "!", _), e)) -> e
+                    | _ -> nop
+                | AppExp(AppExp(FreeVarExp(ValName f, _), x), y) ->
+                    match (f, x, y) with
+                    | ("add", IntExp a, IntExp b) -> IntExp(a + b)
+                    | ("sub", IntExp a, IntExp b) -> IntExp(a - b)
+                    | ("mul", IntExp a, IntExp b) -> IntExp(a * b)
+                    | ("div", IntExp a, IntExp b) -> IntExp(a / b)
+                    | ("mod", IntExp a, IntExp b) -> IntExp(a % b)
+                    | ("pow", IntExp a, IntExp b) -> IntExp(a ** int (b))
+                    | ("add", e, IntExp n) when n = 0I -> e
+                    | ("sub", e, IntExp n) when n = 0I -> e
+                    | ("mul", e, IntExp n) when n = 1I -> e
+                    | ("mul", _, IntExp n) when n = 0I -> IntExp 0I
+                    | ("mul", e, IntExp n) when n = -1I -> go (app (fvar "negate") e)
+                    | ("div", e, IntExp n) when n = 1I -> e
+                    | ("div", e, IntExp n) when n = -1I -> go (app (fvar "negate") e)
+                    | ("mod", _, IntExp n) when n = 1I -> IntExp 0I
+                    | ("pow", e, IntExp n) when n = 1I -> e
+                    | ("add", IntExp n, e) when n = 0I -> e
+                    | ("sub", IntExp n, e) when n = 0I -> go (app (fvar "negate") e)
+                    | ("mul", IntExp n, e) when n = 1I -> e
+                    | ("mul", IntExp n, _) when n = 0I -> IntExp 0I
+                    | ("mul", IntExp n, e) when n = -1I -> go (app (fvar "negate") e)
+                    | ("div", IntExp n, _) when n = 0I -> IntExp 0I
+                    | ("div", IntExp n, e) when n = -1I -> go (app (fvar "negate") e)
+                    | ("mod", IntExp n, _) when n = 0I -> IntExp 0I
+                    | ("pow", IntExp n, _) when n = 1I -> IntExp 1I
+                    | ("&&", BoolExp a, BoolExp b) -> BoolExp(a && b)
+                    | ("||", BoolExp a, BoolExp b) -> BoolExp(a || b)
+                    | ("&&", BoolExp true, e) -> e
+                    | ("&&", BoolExp false, _) -> BoolExp false
+                    | ("||", BoolExp true, _) -> BoolExp true
+                    | ("||", BoolExp false, e) -> e
+                    | ("&&", e, BoolExp true) -> e
+                    | ("&&", _, BoolExp false) -> BoolExp false
+                    | ("||", _, BoolExp true) -> BoolExp true
+                    | ("||", e, BoolExp false) -> e
+                    | ("<", IntExp a, IntExp b) -> BoolExp(a < b)
+                    | ("<=", IntExp a, IntExp b) -> BoolExp(a <= b)
+                    | ("=", IntExp a, IntExp b) -> BoolExp(a = b)
+                    | ("<>", IntExp a, IntExp b) -> BoolExp(a <> b)
+                    | (">=", IntExp a, IntExp b) -> BoolExp(a >= b)
+                    | (">", IntExp a, IntExp b) -> BoolExp(a > b)
+                    | ("=", e, IntExp n) when n = 0I -> go (appf "!" (appf "zahlToBool" e))
+                    | ("=", IntExp n, e) when n = 0I -> go (appf "!" (appf "zahlToBool" e))
+                    | ("<>", e, IntExp n) when n = 0I -> go (appf "zahlToBool" e)
+                    | ("<>", IntExp n, e) when n = 0I -> go (appf "zahlToBool" e)
+                    | ("count", IntExp n, _) when n = 0I -> IntExp 0I
+                    | ("count", n, LamExp(_, e)) when not (findBoundVar 0 e) -> go (mul n (appf "boolToZahl" (unshiftLambda e)))
+                    | ("sum", IntExp n, _) when n = 0I -> IntExp 0I
+                    | ("sum", n, LamExp(_, e)) when not (findBoundVar 0 e) ->
+                        eprintfn "before: %A" nop
+                        eprintfn "after: %A" (mul n (unshiftLambda e))
+                        eprintfn "finally: %A" (go (mul n (unshiftLambda e)))
+                        go (mul n (unshiftLambda e))
+                    | ("sum", n, LamExp(t, AppExp(AppExp(FreeVarExp(ValName op, _), e1), e2))) when op = "+" || op = "-" ->
+                        let e1 = app2f "sum" n (LamExp(t, e1))
+                        let e2 = app2f "sum" n (LamExp(t, e2))
+                        go (app2f op e1 e2)
+                    | ("sum", n, LamExp(t, AppExp(AppExp(FreeVarExp(ValName "*", _), e1), e2))) when not (findBoundVar 0 e1) ->
+                        let e1 = unshiftLambda e1
+                        let e2 = app2f "sum" n (LamExp(t, e2))
+                        go (app2f "*" e1 e2)
+                    | ("sum", n, LamExp(t, AppExp(AppExp(FreeVarExp(ValName "*", _), e1), e2))) when not (findBoundVar 0 e2) ->
+                        let e1 = app2f "sum" n (LamExp(t, e1))
+                        let e2 = unshiftLambda e2
+                        go (app2f "*" e1 e2)
+                    | (maxlike, n, LamExp(t, e)) when (maxlike = "max" || maxlike = "min") && not (findBoundVar 0 e) -> unshiftLambda e
+                    | (maxlike, n, LamExp(t, AppExp(AppExp(FreeVarExp(ValName op, _), e1), e2))) when (maxlike = "max" || maxlike = "min") && (op = "+" || op = "-") && not (findBoundVar 0 e1) ->
+                        let e1 = unshiftLambda e1
+                        let e2 = app2f maxlike n (LamExp(t, e2))
+                        go (app2f op e1 e2)
+                    | (maxlike, n, LamExp(t, AppExp(AppExp(FreeVarExp(ValName op, _), e1), e2))) when (maxlike = "max" || maxlike = "min") && (op = "+" || op = "-") && not (findBoundVar 0 e2) ->
+                        let e1 = app2f maxlike n (LamExp(t, e1))
+                        let e2 = unshiftLambda e2
+                        go (app2f op e1 e2)
+                    | _ -> nop
+                | _ -> nop
             match t1 with
             | FunRTy(_, t3) -> (y, t3)
             | _ -> failwith "failed to construct a type"
