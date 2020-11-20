08236ae9fd9671ba03e2e19538d3ea23703d3cae
Kimiyuki Onaka <kimiyuki95@gmail.com>
2020-11-21 00:06:33 +0900

    Generalize the {de,}serializer with Read/Show

--- src/Jikka/Deserializer/Read.hs
+++ src/Jikka/Deserializer/Read.hs
@@ -1,8 +1,7 @@
 module Jikka.Deserializer.Read where
 
 import Data.Text (Text, unpack)
-import Jikka.Language.Python.Typed.Type (Program (..))
 import Text.Read (readEither)
 
-run :: FilePath -> Text -> Either String Program
+run :: Read program => FilePath -> Text -> Either String program
 run path input = readEither $ unpack input
--- src/Jikka/Serializer/Show.hs
+++ src/Jikka/Serializer/Show.hs
@@ -1,7 +1,6 @@
 module Jikka.Serializer.Show where
 
 import Data.Text (Text, pack)
-import Jikka.Language.Python.Typed.Type (Program (..))
 
-run :: Program -> Either String Text
+run :: Show program => program -> Either String Text
 run e = Right . pack $ show e
