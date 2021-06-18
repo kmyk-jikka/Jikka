5b79a87d7d01a8b2b2a1091d9c1d5626be73c66d
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-06-18 09:48:20 +0900

    Add some utils to src/Jikka/RestrictedPython/Util.hs

--- src/Jikka/RestrictedPython/Util.hs
+++ src/Jikka/RestrictedPython/Util.hs
@@ -2,6 +2,7 @@
 
 module Jikka.RestrictedPython.Util where
 
+import Data.List (delete)
 import Jikka.Common.Alpha
 import Jikka.RestrictedPython.Language.Expr
 
@@ -18,3 +19,26 @@ freeTyVars = \case
   ListTy t -> freeTyVars t
   TupleTy ts -> concat $ mapM freeTyVars ts
   CallableTy ts ret -> concat $ mapM freeTyVars (ret : ts)
+
+freeVars :: Expr -> [VarName]
+freeVars = \case
+  BoolOp e1 _ e2 -> freeVars e1 ++ freeVars e2
+  BinOp e1 _ e2 -> freeVars e1 ++ freeVars e2
+  UnaryOp _ e -> freeVars e
+  Lambda args e -> foldl (\vars (x, _) -> delete x vars) (freeVars e) args
+  IfExp e1 e2 e3 -> freeVars e1 ++ freeVars e2 ++ freeVars e3
+  ListComp e (Comprehension x iter pred) -> freeVars iter ++ foldl (\vars x -> delete x vars) (freeVars e ++ concatMap freeVars pred) (targetVars x)
+  Compare e1 _ e2 -> freeVars e1 ++ freeVars e2
+  Call f args -> concatMap freeVars (f : args)
+  Constant _ -> []
+  Subscript e1 e2 -> freeVars e1 ++ freeVars e2
+  Name x -> [x]
+  List _ es -> concatMap freeVars es
+  Tuple es -> concatMap freeVars es
+  SubscriptSlice e from to step -> freeVars e ++ concatMap freeVars from ++ concatMap freeVars to ++ concatMap freeVars step
+
+targetVars :: Target -> [VarName]
+targetVars = \case
+  SubscriptTrg x _ -> targetVars x
+  NameTrg x -> [x]
+  TupleTrg xs -> concatMap targetVars xs
