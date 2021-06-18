ba8692d685af9f71e800045c0def448e3ba78244
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-06-18 09:23:03 +0900

    Refactor src/Jikka/RestrictedPython/Convert/Alpha.hs

--- src/Jikka/RestrictedPython/Convert/Alpha.hs
+++ src/Jikka/RestrictedPython/Convert/Alpha.hs
@@ -6,6 +6,7 @@ module Jikka.RestrictedPython.Convert.Alpha
   )
 where
 
+import Control.Monad.State.Strict
 import qualified Data.Set as S
 import Jikka.Common.Alpha
 import Jikka.Common.Error
@@ -20,151 +21,147 @@ rename x = do
   let base = if unVarName x == "_" then "" else takeWhile (/= '$') (unVarName x)
   return $ VarName (base ++ '$' : show i)
 
-push :: VarName -> VarName -> Env -> Env
-push x y env
-  | unVarName x == "_" = env
-  | otherwise = (x, y) : env
+push :: MonadState Env m => VarName -> VarName -> m ()
+push x y
+  | unVarName x == "_" = return ()
+  | otherwise = modify' ((x, y) :)
 
-push' :: [(VarName, VarName, a)] -> Env -> Env
-push' xys env = foldl (\env (x, y, _) -> push x y env) env xys
+push' :: MonadState Env m => [(VarName, VarName, a)] -> m ()
+push' xys = mapM_ (\(x, y, _) -> push x y) xys
 
-lookup' :: MonadError Error m => VarName -> Env -> m VarName
-lookup' x env = case lookup x env of
-  Just y -> return y
-  Nothing -> throwSymbolError $ "undefined identifier: " ++ unVarName x
+lookup' :: (MonadState Env m, MonadError Error m) => VarName -> m VarName
+lookup' x = do
+  env <- get
+  case lookup x env of
+    Just y -> return y
+    Nothing -> throwSymbolError $ "undefined identifier: " ++ unVarName x
 
-runTarget :: (MonadAlpha m, MonadError Error m) => Env -> Target -> m (Target, Env)
-runTarget env = \case
+runTarget :: (MonadState Env m, MonadAlpha m, MonadError Error m) => Target -> m Target
+runTarget = \case
   SubscriptTrg f index -> do
-    (f, env) <- runTarget env f
-    index <- runExpr env index
-    return (SubscriptTrg f index, env)
+    f <- runTarget f
+    index <- runExpr index
+    return $ SubscriptTrg f index
   NameTrg x -> do
     y <- rename x
-    return (NameTrg y, push x y env)
+    push x y
+    return $ NameTrg y
   TupleTrg xs -> do
-    let go env [] = return ([], env)
-        go env (x : xs) = do
-          (y, env) <- runTarget env x
-          (ys, env) <- go env xs
-          return (y : ys, env)
-    (ys, env) <- go env xs
-    return (TupleTrg ys, env)
+    let go [] = return []
+        go (x : xs) = do
+          y <- runTarget x
+          ys <- go xs
+          return $ y : ys
+    ys <- go xs
+    return $ TupleTrg ys
 
-runExpr :: (MonadAlpha m, MonadError Error m) => Env -> Expr -> m Expr
-runExpr env = \case
-  BoolOp e1 op e2 -> BoolOp <$> runExpr env e1 <*> return op <*> runExpr env e2
-  BinOp e1 op e2 -> BinOp <$> runExpr env e1 <*> return op <*> runExpr env e2
-  UnaryOp op e -> UnaryOp op <$> runExpr env e
+runExpr :: (MonadState Env m, MonadAlpha m, MonadError Error m) => Expr -> m Expr
+runExpr = \case
+  BoolOp e1 op e2 -> BoolOp <$> runExpr e1 <*> return op <*> runExpr e2
+  BinOp e1 op e2 -> BinOp <$> runExpr e1 <*> return op <*> runExpr e2
+  UnaryOp op e -> UnaryOp op <$> runExpr e
   Lambda args body -> do
     args <- forM args $ \(x, t) -> do
       y <- rename x
       return (x, y, t)
-    let env' = push' args env
+    savedEnv <- get
+    push' args
     let args' = map (\(_, y, t) -> (y, t)) args
-    body <- runExpr env' body
+    body <- runExpr body
+    put savedEnv
     return $ Lambda args' body
   IfExp e1 e2 e3 -> do
-    e1 <- runExpr env e1
-    e2 <- runExpr env e2
-    e3 <- runExpr env e3
+    e1 <- runExpr e1
+    e2 <- runExpr e2
+    e3 <- runExpr e3
     return $ IfExp e1 e2 e3
   ListComp e (Comprehension x iter ifs) -> do
-    iter <- runExpr env iter
-    (y, env) <- runTarget env x
-    ifs <- mapM (runExpr env) ifs
-    e <- runExpr env e
+    iter <- runExpr iter
+    y <- runTarget x
+    ifs <- mapM runExpr ifs
+    e <- runExpr e
     return $ ListComp e (Comprehension y iter ifs)
-  Compare e1 op e2 -> Compare <$> runExpr env e1 <*> return op <*> runExpr env e2
+  Compare e1 op e2 -> Compare <$> runExpr e1 <*> return op <*> runExpr e2
   Call f args -> do
-    f <- runExpr env f
-    args <- mapM (runExpr env) args
+    f <- runExpr f
+    args <- mapM runExpr args
     return $ Call f args
   Constant const -> return $ Constant const
-  Subscript e1 e2 -> Subscript <$> runExpr env e1 <*> runExpr env e2
-  Name x -> Name <$> lookup' x env
-  List t es -> List t <$> mapM (runExpr env) es
-  Tuple es -> Tuple <$> mapM (runExpr env) es
+  Subscript e1 e2 -> Subscript <$> runExpr e1 <*> runExpr e2
+  Name x -> Name <$> lookup' x
+  List t es -> List t <$> mapM runExpr es
+  Tuple es -> Tuple <$> mapM runExpr es
   SubscriptSlice e from to step -> do
-    e <- runExpr env e
-    from <- mapM (runExpr env) from
-    to <- mapM (runExpr env) to
-    step <- mapM (runExpr env) step
+    e <- runExpr e
+    from <- mapM runExpr from
+    to <- mapM runExpr to
+    step <- mapM runExpr step
     return $ SubscriptSlice e from to step
 
-runStatement :: (MonadAlpha m, MonadError Error m) => Env -> Statement -> m (Statement, Env)
-runStatement env = \case
+runStatement :: (MonadState Env m, MonadAlpha m, MonadError Error m) => Statement -> m Statement
+runStatement = \case
   Return e -> do
-    e <- runExpr env e
-    return (Return e, env)
+    e <- runExpr e
+    return $ Return e
   AugAssign x op e -> do
-    e <- runExpr env e
-    (x, env) <- runTarget env x
-    return (AugAssign x op e, env)
+    e <- runExpr e
+    x <- runTarget x
+    return $ AugAssign x op e
   AnnAssign x t e -> do
-    e <- runExpr env e
-    (x, env) <- runTarget env x
-    return (AnnAssign x t e, env)
+    e <- runExpr e
+    x <- runTarget x
+    return $ AnnAssign x t e
   For x e body -> do
-    (y, env) <- runTarget env x
-    e <- runExpr env e
-    (body, _) <- runStatements env body
-    return (For y e body, env)
+    y <- runTarget x
+    e <- runExpr e
+    savedEnv <- get
+    body <- runStatements body
+    put savedEnv
+    return $ For y e body
   If e body1 body2 -> do
-    e <- runExpr env e
-    (body1, _) <- runStatements env body1
-    (body2, _) <- runStatements env body2
-    return (If e body1 body2, env)
+    e <- runExpr e
+    savedEnv <- get
+    body1 <- runStatements body1
+    put savedEnv
+    body2 <- runStatements body2
+    put savedEnv
+    return $ If e body1 body2
   Assert e -> do
-    e <- runExpr env e
-    return (Assert e, env)
+    e <- runExpr e
+    return $ Assert e
 
-mapM' :: (MonadAlpha m, MonadError Error m) => (Env -> a -> m (b, Env)) -> Env -> [a] -> m ([b], Env)
-mapM' f env = \case
-  [] -> return ([], env)
-  (x : xs) -> do
-    y <- catchError' $ f env x
-    case y of
-      Left err -> do
-        ys <- catchError' $ mapM' f env xs
-        case ys of
-          Left err' -> throwError (err <> err')
-          Right _ -> throwError err
-      Right (y, env) -> do
-        (ys, env) <- mapM' f env xs
-        return (y : ys, env)
+runStatements :: (MonadState Env m, MonadAlpha m, MonadError Error m) => [Statement] -> m [Statement]
+runStatements = mapM runStatement
 
-runStatements :: (MonadAlpha m, MonadError Error m) => Env -> [Statement] -> m ([Statement], Env)
-runStatements = mapM' runStatement
-
-runToplevelStatement :: (MonadAlpha m, MonadError Error m) => Env -> ToplevelStatement -> m (ToplevelStatement, Env)
-runToplevelStatement env = \case
+runToplevelStatement :: (MonadState Env m, MonadAlpha m, MonadError Error m) => ToplevelStatement -> m ToplevelStatement
+runToplevelStatement = \case
   ToplevelAnnAssign x t e -> do
     y <- rename x
-    let env' = push x y env
-    e <- runExpr env' e
-    return (ToplevelAnnAssign y t e, env')
+    push x y
+    e <- runExpr e
+    return $ ToplevelAnnAssign y t e
   ToplevelFunctionDef f args ret body -> do
     g <- rename f
-    let env' = push f g env
+    push f g
     args <- forM args $ \(x, t) -> do
       y <- rename x
       return (x, y, t)
     let args' = map (\(_, y, t) -> (y, t)) args
-    let env'' = push' args env'
-    (body, _) <- runStatements env'' body
-    return (ToplevelFunctionDef g args' ret body, env')
+    savedEnv <- get
+    push' args
+    body <- runStatements body
+    put savedEnv
+    return $ ToplevelFunctionDef g args' ret body
   ToplevelAssert e -> do
-    e <- runExpr env e
-    return (ToplevelAssert e, env)
+    e <- runExpr e
+    return $ ToplevelAssert e
 
-runProgram :: (MonadAlpha m, MonadError Error m) => Env -> Program -> m (Program, Env)
-runProgram = mapM' runToplevelStatement
+runProgram :: (MonadState Env m, MonadAlpha m, MonadError Error m) => Program -> m Program
+runProgram = mapM runToplevelStatement
 
 initialEnv :: Env
 initialEnv = map (\x -> (x, x)) (S.toList builtinFunctions)
 
 run :: (MonadAlpha m, MonadError Error m) => Program -> m Program
 run prog = wrapError' "Jikka.RestrictedPython.Convert.Alpha" $ do
-  (prog, _) <- runProgram initialEnv prog
-  return prog
+  evalStateT (runProgram prog) initialEnv
