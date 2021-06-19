3138c2de38f086aa6b474a9fcb428e6d76ecdf2f
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-06-19 14:23:51 +0900

    Make src/Jikka/RestrictedPython/Language/VariableAnalysis.hs

--- src/Jikka/RestrictedPython/Convert/SplitLoops.hs
+++ src/Jikka/RestrictedPython/Convert/SplitLoops.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE LambdaCase #-}
 
 module Jikka.RestrictedPython.Convert.SplitLoops
   ( run,
@@ -8,63 +7,13 @@ module Jikka.RestrictedPython.Convert.SplitLoops
   )
 where
 
-import Data.List (delete, intersect, nub, partition)
+import Data.List (partition)
 import Jikka.Common.Alpha
 import Jikka.Common.Error
 import qualified Jikka.RestrictedPython.Convert.Alpha as Alpha (run)
 import Jikka.RestrictedPython.Language.Expr
 import Jikka.RestrictedPython.Language.Util
-
-newtype ReadList = ReadList [VarName]
-  deriving (Eq, Ord, Show, Read)
-
-newtype WriteList = WriteList [VarName]
-  deriving (Eq, Ord, Show, Read)
-
-hasEdge :: WriteList -> ReadList -> Bool
-hasEdge (WriteList w) (ReadList r) = not (null (w `intersect` r))
-
-analyzeExpr :: Expr -> ReadList
-analyzeExpr = ReadList . freeVars
-
-analyzeTargetRead :: Target -> ReadList
-analyzeTargetRead = ReadList . freeVarsTarget
-
-analyzeTargetWrite :: Target -> WriteList
-analyzeTargetWrite = WriteList . targetVars
-
-analyzeStatement :: Statement -> (ReadList, WriteList)
-analyzeStatement = \case
-  Return e -> (analyzeExpr e, WriteList [])
-  AugAssign x _ e ->
-    let w = analyzeTargetWrite x
-        (ReadList r) = analyzeTargetRead x
-        (ReadList r') = analyzeExpr e
-     in (ReadList (r ++ r'), w)
-  AnnAssign x _ e ->
-    let w = analyzeTargetWrite x
-        (ReadList r) = analyzeTargetRead x
-        (ReadList r') = analyzeExpr e
-     in (ReadList (r ++ r'), w)
-  For x iter body ->
-    let xs = targetVars x
-        ReadList r = analyzeExpr iter
-        (ReadList r', WriteList w) = analyzeStatements body
-     in (ReadList (r ++ foldl (flip delete) r' xs), WriteList (foldl (flip delete) w xs))
-  If e body1 body2 ->
-    let ReadList r = analyzeExpr e
-        (ReadList r1, WriteList w1) = analyzeStatements body1
-        (ReadList r2, WriteList w2) = analyzeStatements body2
-     in (ReadList (r ++ r1 ++ r2), WriteList (w1 ++ w2))
-  Assert e -> (analyzeExpr e, WriteList [])
-
-analyzeStatements :: [Statement] -> (ReadList, WriteList)
-analyzeStatements = go [] []
-  where
-    go r w [] = (ReadList (nub r), WriteList (nub w))
-    go r w (stmt : stmts) =
-      let (ReadList r', WriteList w') = analyzeStatement stmt
-       in go (r' ++ r) (w' ++ w) stmts
+import Jikka.RestrictedPython.Language.VariableAnalysis
 
 -- | `runForLoop` splits a for-loop to many for-loops as possible.
 -- This assumes that `hasNoSubscriptionInLoopCounters`, `hasNoAssignToLoopCounters`, and `hasNoAssignToLoopIterators` hold.
@@ -72,7 +21,7 @@ analyzeStatements = go [] []
 -- This function analyzes read-variables and write-variables in statements, and split statements into connected components.
 runForLoop :: Target -> Expr -> [Statement] -> [Statement]
 runForLoop x iter body =
-  let connected (_, (r, w)) (_, (r', w')) = hasEdge w r' || hasEdge w' r
+  let connected (_, (r, w)) (_, (r', w')) = haveIntersection w r' || haveIntersection w' r
       go result [] = reverse result
       go result (stmt : stmts) =
         let (same, diff) = partition (connected stmt) stmts
---
+++ src/Jikka/RestrictedPython/Language/VariableAnalysis.hs
@@ -0,0 +1,58 @@
+{-# LANGUAGE LambdaCase #-}
+
+module Jikka.RestrictedPython.Language.VariableAnalysis where
+
+import Data.List (delete, intersect, nub)
+import Jikka.RestrictedPython.Language.Expr
+import Jikka.RestrictedPython.Language.Util
+
+newtype ReadList = ReadList [VarName]
+  deriving (Eq, Ord, Show, Read)
+
+newtype WriteList = WriteList [VarName]
+  deriving (Eq, Ord, Show, Read)
+
+haveIntersection :: WriteList -> ReadList -> Bool
+haveIntersection (WriteList w) (ReadList r) = not (null (w `intersect` r))
+
+analyzeExpr :: Expr -> ReadList
+analyzeExpr = ReadList . freeVars
+
+analyzeTargetRead :: Target -> ReadList
+analyzeTargetRead = ReadList . freeVarsTarget
+
+analyzeTargetWrite :: Target -> WriteList
+analyzeTargetWrite = WriteList . targetVars
+
+analyzeStatement :: Statement -> (ReadList, WriteList)
+analyzeStatement = \case
+  Return e -> (analyzeExpr e, WriteList [])
+  AugAssign x _ e ->
+    let w = analyzeTargetWrite x
+        (ReadList r) = analyzeTargetRead x
+        (ReadList r') = analyzeExpr e
+     in (ReadList (r ++ r'), w)
+  AnnAssign x _ e ->
+    let w = analyzeTargetWrite x
+        (ReadList r) = analyzeTargetRead x
+        (ReadList r') = analyzeExpr e
+     in (ReadList (r ++ r'), w)
+  For x iter body ->
+    let xs = targetVars x
+        ReadList r = analyzeExpr iter
+        (ReadList r', WriteList w) = analyzeStatements body
+     in (ReadList (r ++ foldl (flip delete) r' xs), WriteList (foldl (flip delete) w xs))
+  If e body1 body2 ->
+    let ReadList r = analyzeExpr e
+        (ReadList r1, WriteList w1) = analyzeStatements body1
+        (ReadList r2, WriteList w2) = analyzeStatements body2
+     in (ReadList (r ++ r1 ++ r2), WriteList (w1 ++ w2))
+  Assert e -> (analyzeExpr e, WriteList [])
+
+analyzeStatements :: [Statement] -> (ReadList, WriteList)
+analyzeStatements = go [] []
+  where
+    go r w [] = (ReadList (nub r), WriteList (nub w))
+    go r w (stmt : stmts) =
+      let (ReadList r', WriteList w') = analyzeStatement stmt
+       in go (r' ++ r) (w' ++ w) stmts
