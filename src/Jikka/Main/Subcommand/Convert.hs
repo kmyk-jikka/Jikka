7ba697c42f5ff93b122b6573ae4433b555f7d40d
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-06-23 07:45:29 +0900

    feat: Add --target option

--- src/Jikka/Main/Subcommand/Convert.hs
+++ src/Jikka/Main/Subcommand/Convert.hs
@@ -1,20 +1,52 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Jikka.Main.Subcommand.Convert (run) where
 
-import Data.Text (Text)
+import Data.Text (Text, pack)
 import qualified Jikka.CPlusPlus.Convert as FromCore
 import qualified Jikka.CPlusPlus.Format as FormatCPlusPlus
 import Jikka.Common.Alpha
 import Jikka.Common.Error
+import qualified Jikka.Core.Format as FormatCore
 import qualified Jikka.Core.Optimize as Optimize
+import Jikka.Main.Target
 import qualified Jikka.Python.Convert.ToRestrictedPython as ToRestrictedPython
-import qualified Jikka.Python.Parse as FromPython
+import qualified Jikka.Python.Parse as ParsePython
 import qualified Jikka.RestrictedPython.Convert as ToCore
+import qualified Jikka.RestrictedPython.Format as FormatRestrictedPython
 
-run :: FilePath -> Text -> Either Error Text
-run path input = flip evalAlphaT 0 $ do
-  prog <- FromPython.run path input
+runPython :: FilePath -> Text -> Either Error Text
+runPython path input = flip evalAlphaT 0 $ do
+  prog <- ParsePython.run path input
+  return . pack $ show prog -- TODO
+
+runRestrictedPython :: FilePath -> Text -> Either Error Text
+runRestrictedPython path input = flip evalAlphaT 0 $ do
+  prog <- ParsePython.run path input
+  prog <- ToRestrictedPython.run prog
+  prog <- ToCore.run' prog
+  FormatRestrictedPython.run prog
+
+runCore :: FilePath -> Text -> Either Error Text
+runCore path input = flip evalAlphaT 0 $ do
+  prog <- ParsePython.run path input
+  prog <- ToRestrictedPython.run prog
+  prog <- ToCore.run prog
+  prog <- Optimize.run prog
+  FormatCore.run prog
+
+runCPlusPlus :: FilePath -> Text -> Either Error Text
+runCPlusPlus path input = flip evalAlphaT 0 $ do
+  prog <- ParsePython.run path input
   prog <- ToRestrictedPython.run prog
   prog <- ToCore.run prog
   prog <- Optimize.run prog
   prog <- FromCore.run prog
   FormatCPlusPlus.run prog
+
+run :: Target -> FilePath -> Text -> Either Error Text
+run = \case
+  PythonTarget -> runPython
+  RestrictedPythonTarget -> runRestrictedPython
+  CoreTarget -> runCore
+  CPlusPlusTarget -> runCPlusPlus
--- src/Jikka/Main/Subcommand/Execute.hs
+++ src/Jikka/Main/Subcommand/Execute.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Jikka.Main.Subcommand.Execute (run) where
@@ -6,20 +7,43 @@ import Control.Monad.Except
 import qualified Data.Text.IO as T (readFile)
 import Jikka.Common.Alpha
 import Jikka.Common.Error
+import qualified Jikka.Core.Evaluate as EvaluateCore
+import Jikka.Main.Target
 import qualified Jikka.Python.Convert.ToRestrictedPython as ToRestrictedPython
 import qualified Jikka.Python.Parse as FromPython
-import qualified Jikka.RestrictedPython.Convert as Convert
-import qualified Jikka.RestrictedPython.Evaluate as Evaluate
-import qualified Jikka.RestrictedPython.Language.Value as Value
+import qualified Jikka.RestrictedPython.Convert as ToCore
+import qualified Jikka.RestrictedPython.Evaluate as EvaluateRestrictedPython
+import qualified Jikka.RestrictedPython.Language.Value as ValueRestrictedPythong
 
-run :: FilePath -> ExceptT Error IO ()
-run path = flip evalAlphaT 0 $ do
+runPython :: FilePath -> ExceptT Error IO ()
+runPython _ = throwCommandLineError "cannot execute Python"
+
+runRestrictedPython :: FilePath -> ExceptT Error IO ()
+runRestrictedPython path = flip evalAlphaT 0 $ do
   prog <- liftIO $ T.readFile path
   prog <- liftEither $ FromPython.run path prog
   prog <- ToRestrictedPython.run prog
-  -- TODO: convert it to core
-  prog <- Convert.run' prog
-  global <- Evaluate.makeGlobal prog
-  entrypoint <- Value.makeEntryPointIO "solve" global
-  value <- Evaluate.runWithGlobal global entrypoint
-  liftIO $ Value.writeValueIO value
+  prog <- ToCore.run' prog
+  global <- EvaluateRestrictedPython.makeGlobal prog
+  entrypoint <- ValueRestrictedPythong.makeEntryPointIO "solve" global
+  value <- EvaluateRestrictedPython.runWithGlobal global entrypoint
+  liftIO $ ValueRestrictedPythong.writeValueIO value
+
+runCore :: FilePath -> ExceptT Error IO ()
+runCore path = flip evalAlphaT 0 $ do
+  prog <- liftIO $ T.readFile path
+  prog <- liftEither $ FromPython.run path prog
+  prog <- ToRestrictedPython.run prog
+  prog <- ToCore.run prog
+  value <- EvaluateCore.run prog
+  liftIO $ print value
+
+runCPlusPlus :: FilePath -> ExceptT Error IO ()
+runCPlusPlus _ = throwCommandLineError "cannot execute C++"
+
+run :: Target -> FilePath -> ExceptT Error IO ()
+run = \case
+  PythonTarget -> runPython
+  RestrictedPythonTarget -> runRestrictedPython
+  CoreTarget -> runCore
+  CPlusPlusTarget -> runCPlusPlus
