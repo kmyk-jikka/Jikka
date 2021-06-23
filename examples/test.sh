1f77ecbec86208a92dfbe7b3645268ed004aef7b
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-06-23 13:10:02 +0900

    test: Now execution of core language works!

--- examples/test.sh
+++ examples/test.sh
@@ -2,5 +2,5 @@
 set -ex
 for f in examples/*.in ; do
     diff <(stack --system-ghc run -- execute --target rpython ${f%.in}.py < $f) ${f%.in}.out
-    # diff <(stack --system-ghc run -- execute --target core ${f%.in}.py < $f) ${f%.in}.out
+    diff <(stack --system-ghc run -- execute --target core ${f%.in}.py < $f) ${f%.in}.out
 done
