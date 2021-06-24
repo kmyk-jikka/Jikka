a6d599360ba5aa03f7f607b41be092d300c93215
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-06-25 08:52:17 +0900

    test: Make integration tests harder

--- examples/fib.py
+++ examples/fib.py
@@ -1,8 +1,11 @@
-def solve(n: int) -> int:
+def f(n: int) -> int:
     a = 0
     b = 1
     for _ in range(n):
         c = a + b
         a = b
         b = c
-    return a
+    return a % 1000000007
+
+def solve(n: int) -> int:
+    return f(n)
