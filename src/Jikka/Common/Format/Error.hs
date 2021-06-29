5baee21b18efff4f3de199a0e35166c5e2649495
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-06-29 21:04:49 +0900

    feat: Use colors in error messages

--- src/Jikka/Common/Format/Error.hs
+++ src/Jikka/Common/Format/Error.hs
@@ -4,14 +4,18 @@
 module Jikka.Common.Format.Error
   ( prettyError,
     prettyErrorWithText,
+    hPrintError,
+    hPrintErrorWithText,
   )
 where
 
 import Data.List (intercalate)
 import Data.Text (Text)
 import Jikka.Common.Error
+import Jikka.Common.Format.Color
 import Jikka.Common.Format.Location
 import Jikka.Common.Location
+import System.IO (Handle, hPutStrLn)
 
 -- | `unpackCombinedErrors` removes `ErrorAppend` ctor from the given `Error`.
 unpackCombinedErrors :: Error -> [Error]
@@ -20,20 +24,20 @@ unpackCombinedErrors = go
     go :: Error -> [Error]
     go = \case
       err@(Error _) -> [err]
-      ErrorAppend err1 err2 -> unpackCombinedErrors err1 ++ unpackCombinedErrors err2
+      ErrorAppend err1 err2 -> go err1 ++ go err2
       WithGroup group err -> map (WithGroup group) (go err)
       WithWrapped msg err -> map (WithWrapped msg) (go err)
       WithLocation loc err -> map (WithLocation loc) (go err)
       WithResponsibility resp err -> map (WithResponsibility resp) (go err)
 
-prettyError :: Error -> [String]
-prettyError = map prettyError1 . unpackCombinedErrors
+prettyError :: ColorFlag -> Error -> [String]
+prettyError color = map (prettyError1 color) . unpackCombinedErrors
 
 -- | @err@ must not have `ErrorAppend`.
-prettyError1 :: Error -> String
-prettyError1 err = intercalate ": " ((group ++ loc ++ resp) : getMessages err)
+prettyError1 :: ColorFlag -> Error -> String
+prettyError1 color err = intercalate ": " ((group ++ loc ++ resp) : getMessages err)
   where
-    group = prettyGroup (getErrorGroup err)
+    group = withColor color Red $ prettyGroup (getErrorGroup err)
     loc = case getLocation err of
       Nothing -> ""
       Just loc -> " (" ++ prettyLoc loc ++ ")"
@@ -42,14 +46,14 @@ prettyError1 err = intercalate ": " ((group ++ loc ++ resp) : getMessages err)
       Just ImplementationBug -> " (implementation's bug?)"
       Nothing -> ""
 
-prettyErrorWithText :: Text -> Error -> [String]
-prettyErrorWithText text = intercalate [""] . map (prettyErrorWithText1 text) . unpackCombinedErrors
+prettyErrorWithText :: ColorFlag -> Text -> Error -> [String]
+prettyErrorWithText color text = intercalate [""] . map (prettyErrorWithText1 color text) . unpackCombinedErrors
 
 -- | @err@ must not have `ErrorAppend`.
-prettyErrorWithText1 :: Text -> Error -> [String]
-prettyErrorWithText1 text err = case getLocation err of
-  Nothing -> [prettyError1 err]
-  Just loc -> prettyError1 err : prettyLocWithText text loc
+prettyErrorWithText1 :: ColorFlag -> Text -> Error -> [String]
+prettyErrorWithText1 color text err = case getLocation err of
+  Nothing -> [prettyError1 color err]
+  Just loc -> prettyError1 color err : prettyLocWithText color text loc
 
 prettyGroup :: Maybe ErrorGroup -> String
 prettyGroup = \case
@@ -114,3 +118,13 @@ getResponsibility = \case
   WithWrapped _ err -> getResponsibility err
   WithLocation _ err -> getResponsibility err
   WithResponsibility resp _ -> Just resp
+
+hPrintError :: Handle -> Error -> IO ()
+hPrintError handle err = do
+  color <- hGetColorFlag handle
+  mapM_ (hPutStrLn handle) (prettyError color err)
+
+hPrintErrorWithText :: Handle -> Text -> Error -> IO ()
+hPrintErrorWithText handle text err = do
+  color <- hGetColorFlag handle
+  mapM_ (hPutStrLn handle) (prettyErrorWithText color text err)
--- src/Jikka/Common/Format/Location.hs
+++ src/Jikka/Common/Format/Location.hs
@@ -1,14 +1,19 @@
-module Jikka.Common.Format.Location (prettyLoc, prettyLocWithText) where
+module Jikka.Common.Format.Location
+  ( prettyLoc,
+    prettyLocWithText,
+  )
+where
 
 import Data.Text (Text)
 import qualified Data.Text as T
+import Jikka.Common.Format.Color
 import Jikka.Common.Location
 
 prettyLoc :: Loc -> String
 prettyLoc loc = "line " ++ show (line loc) ++ " column " ++ show (column loc)
 
-prettyLocWithText :: Text -> Loc -> [String]
-prettyLocWithText text (Loc y x width) = result
+prettyLocWithText :: ColorFlag -> Text -> Loc -> [String]
+prettyLocWithText color text (Loc y x width) = result
   where
     lines :: [Text]
     lines = T.lines text
@@ -16,17 +21,17 @@ prettyLocWithText text (Loc y x width) = result
     paddingSize = length $ show (y + 1)
     padRight :: String -> String
     padRight s = s ++ replicate (paddingSize - length s) ' '
-    prettyLine :: Int -> [String]
-    prettyLine y
-      | 1 <= y && y <= length lines = [padRight (show y) ++ " |" ++ T.unpack (lines !! (y - 1))]
+    prettyLine :: (String -> String) -> Int -> [String]
+    prettyLine f y
+      | 1 <= y && y <= length lines = [withColor color Blue (padRight (show y) ++ " |") ++ f (T.unpack (lines !! (y - 1)))]
       | otherwise = []
     result :: [String]
     result
       | 1 <= y && y <= length lines =
         concat
-          [ prettyLine (y - 1),
-            prettyLine y,
-            [replicate (paddingSize + 2 + x - 1) ' ' ++ replicate (max 1 width) '^'],
-            prettyLine (y + 1)
+          [ prettyLine id (y - 1),
+            prettyLine (\line -> take (x - 1) line ++ withColor color Red (take width (drop (x - 1) line)) ++ drop (x - 1 + width) line) y,
+            [replicate (paddingSize + 2 + x - 1) ' ' ++ withColor color Red (replicate (max 1 width) '^')],
+            prettyLine id (y + 1)
           ]
       | otherwise = ["<invalid loc>"]
