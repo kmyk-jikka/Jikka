6959a2d8d2c463dbb69bab739e8b737aa9da0a53
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-06-29 23:42:14 +0900

    test: Add examples/abc127_e.py

---
+++ examples/abc127_e.py
@@ -0,0 +1,13 @@
+# https://atcoder.jp/contests/abc127/tasks/abc127_e
+
+MOD: int = 10 ** 9 + 7
+
+def solve(h: int, w: int, k: int) -> int:
+    ans = 0
+    for y1 in range(h):
+        for x1 in range(w):
+            for y2 in range(h):
+                for x2 in range(w):
+                    if (y1, x1) < (y2, x2):
+                        ans += choose(h * w - 2, k - 2) * (abs(y2 - y1) + abs(x2 - x1))
+    return ans % MOD
