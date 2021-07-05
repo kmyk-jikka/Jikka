25394cafc5c2c5125be8a69dde1dbb894bdaad9e
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-07-06 01:02:27 +0900

    refactor(core): Make a module for rewrite rules

--- src/Jikka/Core/Convert/StrengthReduction.hs
+++ src/Jikka/Core/Convert/StrengthReduction.hs
@@ -22,216 +22,217 @@ import Jikka.Common.Error
 import Jikka.Core.Language.BuiltinPatterns
 import Jikka.Core.Language.Expr
 import Jikka.Core.Language.Lint
+import Jikka.Core.Language.RewriteRules
 import Jikka.Core.Language.Util
 import Jikka.Core.Language.Vars
 
 -- | `eliminateSomeBuiltins` removes some `Builtin` from `Expr` at all.
-eliminateSomeBuiltins :: Expr -> Expr
-eliminateSomeBuiltins = \case
+eliminateSomeBuiltins :: Monad m => RewriteRule m
+eliminateSomeBuiltins = simpleRewriteRule $ \case
   -- arithmetical functions
-  Minus' e1 e2 -> Plus' e1 (Negate' e2)
+  Minus' e1 e2 -> Just $ Plus' e1 (Negate' e2)
   -- advanced arithmetical functions
-  Abs' e -> Max2' IntTy e (Negate' e)
-  Lcm' e1 e2 -> FloorDiv' (Gcd' e1 e2) (Mult' e1 e2)
+  Abs' e -> Just $ Max2' IntTy e (Negate' e)
+  Lcm' e1 e2 -> Just $ FloorDiv' (Gcd' e1 e2) (Mult' e1 e2)
   -- logical functions
-  Implies' e1 e2 -> Or' (Not' e1) e2
+  Implies' e1 e2 -> Just $ Or' (Not' e1) e2
   -- comparison
-  GreaterThan' t e1 e2 -> LessThan' t e2 e1
-  GreaterEqual' t e1 e2 -> LessEqual' t e2 e1
-  NotEqual' t e1 e2 -> Not' (Equal' t e1 e2)
-  e -> e
+  GreaterThan' t e1 e2 -> Just $ LessThan' t e2 e1
+  GreaterEqual' t e1 e2 -> Just $ LessEqual' t e2 e1
+  NotEqual' t e1 e2 -> Just $ Not' (Equal' t e1 e2)
+  _ -> Nothing
 
 -- | `reduceNegate` brings `Negate` to the root.
-reduceNegate :: Expr -> Expr
-reduceNegate = \case
-  Negate' (Negate' e) -> e
-  Plus' (Negate' e1) (Negate' e2) -> Negate' (Plus' e1 e2)
+reduceNegate :: Monad m => RewriteRule m
+reduceNegate = simpleRewriteRule $ \case
+  Negate' (Negate' e) -> Just e
+  Plus' (Negate' e1) (Negate' e2) -> Just $ Negate' (Plus' e1 e2)
   -- `Minus` is already removed.
-  Mult' (Negate' e1) e2 -> Negate' (Mult' e1 e2)
-  Mult' e1 (Negate' e2) -> Negate' (Mult' e1 e2)
+  Mult' (Negate' e1) e2 -> Just $ Negate' (Mult' e1 e2)
+  Mult' e1 (Negate' e2) -> Just $ Negate' (Mult' e1 e2)
   -- `Abs` is already removed.
-  Min2' IntTy (Negate' e1) (Negate' e2) -> Negate' (Max2' IntTy e1 e2)
-  Max2' IntTy (Negate' e1) (Negate' e2) -> Negate' (Min2' IntTy e1 e2)
-  e -> e
+  Min2' IntTy (Negate' e1) (Negate' e2) -> Just $ Negate' (Max2' IntTy e1 e2)
+  Max2' IntTy (Negate' e1) (Negate' e2) -> Just $ Negate' (Min2' IntTy e1 e2)
+  _ -> Nothing
 
 -- | `reduceNot` brings `Not` to the root.
-reduceNot :: Expr -> Expr
-reduceNot = \case
-  Not' (Not' e) -> e
-  And' (Not' e1) (Not' e2) -> Not' (Or' e1 e2)
-  Or' (Not' e1) (Not' e2) -> Not' (And' e1 e2)
+reduceNot :: Monad m => RewriteRule m
+reduceNot = simpleRewriteRule $ \case
+  Not' (Not' e) -> Just e
+  And' (Not' e1) (Not' e2) -> Just $ Not' (Or' e1 e2)
+  Or' (Not' e1) (Not' e2) -> Just $ Not' (And' e1 e2)
   -- `Implies` is already removed.
-  Mult' (Negate' e1) e2 -> Negate' (Mult' e1 e2)
-  Mult' e1 (Negate' e2) -> Negate' (Mult' e1 e2)
-  If' t (Not' e1) e2 e3 -> If' t e1 e3 e2
-  e -> e
+  Mult' (Negate' e1) e2 -> Just $ Negate' (Mult' e1 e2)
+  Mult' e1 (Negate' e2) -> Just $ Negate' (Mult' e1 e2)
+  If' t (Not' e1) e2 e3 -> Just $ If' t e1 e3 e2
+  _ -> Nothing
 
 -- | `reduceBitNot` brings `BitNot` to the root.
-reduceBitNot :: Expr -> Expr
-reduceBitNot = \case
-  BitNot' (BitNot' e) -> e
-  BitAnd' (BitNot' e1) (BitNot' e2) -> BitNot' (BitOr' e1 e2)
-  BitOr' (BitNot' e1) (BitNot' e2) -> BitNot' (BitAnd' e1 e2)
-  BitXor' (BitNot' e1) e2 -> BitNot' (BitXor' e1 e2)
-  BitXor' e1 (BitNot' e2) -> BitNot' (BitXor' e1 e2)
-  e -> e
+reduceBitNot :: Monad m => RewriteRule m
+reduceBitNot = simpleRewriteRule $ \case
+  BitNot' (BitNot' e) -> Just e
+  BitAnd' (BitNot' e1) (BitNot' e2) -> Just $ BitNot' (BitOr' e1 e2)
+  BitOr' (BitNot' e1) (BitNot' e2) -> Just $ BitNot' (BitAnd' e1 e2)
+  BitXor' (BitNot' e1) e2 -> Just $ BitNot' (BitXor' e1 e2)
+  BitXor' e1 (BitNot' e2) -> Just $ BitNot' (BitXor' e1 e2)
+  _ -> Nothing
 
-reduceAssoc :: Expr -> Expr
-reduceAssoc = \case
-  Plus' (Plus' e1 e2) e3 -> Plus' e1 (Plus' e2 e3)
-  Minus' (Minus' e1 e2) e3 -> Minus' e1 (Minus' e2 e3)
-  Mult' (Mult' e1 e2) e3 -> Mult' e1 (Mult' e2 e3)
-  Max2' t1 (Max2' t2 e1 e2) e3 -> Max2' t1 e1 (Max2' t2 e2 e3)
-  Min2' t1 (Min2' t2 e1 e2) e3 -> Min2' t1 e1 (Min2' t2 e2 e3)
-  And' (And' e1 e2) e3 -> And' e1 (And' e2 e3)
-  Or' (Or' e1 e2) e3 -> Or' e1 (Or' e2 e3)
-  BitAnd' (BitAnd' e1 e2) e3 -> BitAnd' e1 (BitAnd' e2 e3)
-  BitOr' (BitOr' e1 e2) e3 -> BitOr' e1 (BitOr' e2 e3)
-  BitXor' (BitXor' e1 e2) e3 -> BitXor' e1 (BitXor' e2 e3)
-  e -> e
+reduceAssoc :: Monad m => RewriteRule m
+reduceAssoc = simpleRewriteRule $ \case
+  Plus' (Plus' e1 e2) e3 -> Just $ Plus' e1 (Plus' e2 e3)
+  Minus' (Minus' e1 e2) e3 -> Just $ Minus' e1 (Minus' e2 e3)
+  Mult' (Mult' e1 e2) e3 -> Just $ Mult' e1 (Mult' e2 e3)
+  Max2' t1 (Max2' t2 e1 e2) e3 -> Just $ Max2' t1 e1 (Max2' t2 e2 e3)
+  Min2' t1 (Min2' t2 e1 e2) e3 -> Just $ Min2' t1 e1 (Min2' t2 e2 e3)
+  And' (And' e1 e2) e3 -> Just $ And' e1 (And' e2 e3)
+  Or' (Or' e1 e2) e3 -> Just $ Or' e1 (Or' e2 e3)
+  BitAnd' (BitAnd' e1 e2) e3 -> Just $ BitAnd' e1 (BitAnd' e2 e3)
+  BitOr' (BitOr' e1 e2) e3 -> Just $ BitOr' e1 (BitOr' e2 e3)
+  BitXor' (BitXor' e1 e2) e3 -> Just $ BitXor' e1 (BitXor' e2 e3)
+  _ -> Nothing
 
 -- | `reduceBuild` converts all other list constucting functions to `Range1`.
-reduceBuild :: MonadAlpha m => Expr -> m Expr
-reduceBuild = \case
-  Tabulate' t n f -> return $ Map' IntTy t f (Range1' n)
+reduceBuild :: MonadAlpha m => RewriteRule m
+reduceBuild = RewriteRule $ \_ -> \case
+  Tabulate' t n f -> return . Just $ Map' IntTy t f (Range1' n)
   Range2' l r -> do
     let n = Minus' r l
     x <- genVarName'
-    return $ Tabulate' IntTy (Lam x IntTy (Plus' l (Var x))) n
+    return . Just $ Tabulate' IntTy (Lam x IntTy (Plus' l (Var x))) n
   Range3' l r step -> do
     let n = CeilDiv' (Minus' r l) step
     x <- genVarName'
-    return $ Tabulate' IntTy (Lam x IntTy (Plus' l (Mult' step (Var x)))) n
-  e -> return e
+    return . Just $ Tabulate' IntTy (Lam x IntTy (Plus' l (Mult' step (Var x)))) n
+  _ -> return Nothing
 
-reduceMapMap :: MonadAlpha m => Expr -> m Expr
-reduceMapMap = \case
+reduceMapMap :: MonadAlpha m => RewriteRule m
+reduceMapMap = RewriteRule $ \_ -> \case
   -- reduce `Reversed`
-  Reversed' _ (Reversed' _ xs) -> return xs
-  Reversed' _ (Map' t1 t2 f xs) -> return $ Map' t1 t2 f (Reversed' t1 xs)
+  Reversed' _ (Reversed' _ xs) -> return $ Just xs
+  Reversed' _ (Map' t1 t2 f xs) -> return . Just $ Map' t1 t2 f (Reversed' t1 xs)
   -- reduce `Sorted`
-  Sorted' t (Reversed' _ xs) -> return $ Sorted' t xs
-  Sorted' t (Sorted' _ xs) -> return $ Sorted' t xs
-  Sorted' _ (Range1' n) -> return $ Range1' n
-  Sorted' _ (Map' t1 t2 f xs) -> return $ Map' t1 t2 f (Sorted' t1 xs)
+  Sorted' t (Reversed' _ xs) -> return . Just $ Sorted' t xs
+  Sorted' t (Sorted' _ xs) -> return . Just $ Sorted' t xs
+  Sorted' _ (Range1' n) -> return . Just $ Range1' n
+  Sorted' _ (Map' t1 t2 f xs) -> return . Just $ Map' t1 t2 f (Sorted' t1 xs)
   -- reduce `Map`
-  Map' _ _ (LamId _ _) xs -> return xs
-  Map' _ t3 f (Map' t1 _ (Lam x t e) xs) -> return $ Map' t1 t3 (Lam x t (App f e)) xs
+  Map' _ _ (LamId _ _) xs -> return $ Just xs
+  Map' _ t3 f (Map' t1 _ (Lam x t e) xs) -> return . Just $ Map' t1 t3 (Lam x t (App f e)) xs
   Map' _ t3 g (Map' t1 _ f xs) -> do
     x <- genVarName'
-    return $ Map' t1 t3 (Lam x t1 (App g (App f (Var x)))) xs
-  e -> return e
+    return . Just $ Map' t1 t3 (Lam x t1 (App g (App f (Var x)))) xs
+  _ -> return Nothing
 
-reduceFoldMap :: Expr -> Expr
-reduceFoldMap = \case
+reduceFoldMap :: Monad m => RewriteRule m
+reduceFoldMap = simpleRewriteRule $ \case
   -- reduce `Reversed`
-  Len' t (Reversed' _ xs) -> Len' t xs
-  At' t (Reversed' _ xs) i -> At' t xs (Minus' (Minus' (Len' t xs) i) Lit1)
-  Sum' (Reversed' _ xs) -> Sum' xs
-  Product' (Reversed' _ xs) -> Product' xs
-  Max1' t (Reversed' _ xs) -> Max1' t xs
-  Min1' t (Reversed' _ xs) -> Min1' t xs
-  ArgMin' t (Reversed' _ xs) -> ArgMin' t xs
-  ArgMax' t (Reversed' _ xs) -> ArgMax' t xs
-  All' (Reversed' _ xs) -> All' xs
-  Any' (Reversed' _ xs) -> Any' xs
+  Len' t (Reversed' _ xs) -> Just $ Len' t xs
+  At' t (Reversed' _ xs) i -> Just $ At' t xs (Minus' (Minus' (Len' t xs) i) Lit1)
+  Sum' (Reversed' _ xs) -> Just $ Sum' xs
+  Product' (Reversed' _ xs) -> Just $ Product' xs
+  Max1' t (Reversed' _ xs) -> Just $ Max1' t xs
+  Min1' t (Reversed' _ xs) -> Just $ Min1' t xs
+  ArgMin' t (Reversed' _ xs) -> Just $ ArgMin' t xs
+  ArgMax' t (Reversed' _ xs) -> Just $ ArgMax' t xs
+  All' (Reversed' _ xs) -> Just $ All' xs
+  Any' (Reversed' _ xs) -> Just $ Any' xs
   -- reduce `Sorted`
-  Len' t (Sorted' _ xs) -> Len' t xs
-  Sum' (Sorted' _ xs) -> Sum' xs
-  Product' (Sorted' _ xs) -> Product' xs
-  Max1' t (Sorted' _ xs) -> Max1' t xs
-  Min1' t (Sorted' _ xs) -> Min1' t xs
-  All' (Sorted' _ xs) -> All' xs
-  Any' (Sorted' _ xs) -> Any' xs
+  Len' t (Sorted' _ xs) -> Just $ Len' t xs
+  Sum' (Sorted' _ xs) -> Just $ Sum' xs
+  Product' (Sorted' _ xs) -> Just $ Product' xs
+  Max1' t (Sorted' _ xs) -> Just $ Max1' t xs
+  Min1' t (Sorted' _ xs) -> Just $ Min1' t xs
+  All' (Sorted' _ xs) -> Just $ All' xs
+  Any' (Sorted' _ xs) -> Just $ Any' xs
   -- reduce `Map`
-  Len' _ (Map' t1 _ _ xs) -> Len' t1 xs
-  At' _ (Map' t1 _ f xs) i -> App f (At' t1 xs i)
-  Sum' (Map' t1 _ (Lam x _ e) xs) | x `isUnusedVar` e -> Mult' (Len' t1 xs) e
-  Sum' (Map' t1 t2 (Lam x t (Negate' e)) xs) -> Negate' (Sum' (Map' t1 t2 (Lam x t e) xs))
-  Sum' (Map' t1 t2 (Lam x t (Plus' e1 e2)) xs) -> Plus' (Sum' (Map' t1 t2 (Lam x t e1) xs)) (Sum' (Map' t1 t2 (Lam x t e2) xs))
-  Sum' (Map' t1 t2 (Lam x t (Mult' e1 e2)) xs) | x `isUnusedVar` e1 -> Mult' e1 (Sum' (Map' t1 t2 (Lam x t e2) xs))
-  Sum' (Map' t1 t2 (Lam x t (Mult' e1 e2)) xs) | x `isUnusedVar` e2 -> Mult' e2 (Sum' (Map' t1 t2 (Lam x t e1) xs))
-  Product' (Map' t1 _ (Lam x _ e) xs) | x `isUnusedVar` e -> Pow' e (Len' t1 xs)
-  Product' (Map' t1 t2 (Lam x t (Negate' e)) xs) -> Mult' (Pow' (Negate' Lit0) (Len' t1 xs)) (Product' (Map' t1 t2 (Lam x t e) xs))
-  Product' (Map' t1 t2 (Lam x t (Mult' e1 e2)) xs) -> Mult' (Product' (Map' t1 t2 (Lam x t e1) xs)) (Product' (Map' t1 t2 (Lam x t e2) xs))
-  Max1' _ (Map' _ _ (Lam x _ e) _) | x `isUnusedVar` e -> e
-  Max1' _ (Map' t1 t2 (Lam x t (Max2' t' e1 e2)) xs) -> Max2' t' (Map' t1 t2 (Lam x t e1) xs) (Map' t1 t2 (Lam x t e2) xs)
-  Max1' _ (Map' t1 t2 (Lam x t (Negate' e)) xs) -> Negate' (Min1' t2 (Map' t1 t2 (Lam x t e) xs))
-  Max1' _ (Map' t1 t2 (Lam x t (Plus' e1 e2)) xs) | x `isUnusedVar` e1 -> Plus' e1 (Max1' t2 (Map' t1 t2 (Lam x t e2) xs))
-  Max1' _ (Map' t1 t2 (Lam x t (Plus' e1 e2)) xs) | x `isUnusedVar` e2 -> Plus' (Max1' t2 (Map' t1 t2 (Lam x t e1) xs)) e1
-  Min1' _ (Map' _ _ (Lam x _ e) _) | x `isUnusedVar` e -> e
-  Min1' _ (Map' t1 t2 (Lam x t (Min2' t' e1 e2)) xs) -> Min2' t' (Map' t1 t2 (Lam x t e1) xs) (Map' t1 t2 (Lam x t e2) xs)
-  Min1' _ (Map' t1 t2 (Lam x t (Negate' e)) xs) -> Negate' (Max1' t2 (Map' t1 t2 (Lam x t e) xs))
-  Min1' _ (Map' t1 t2 (Lam x t (Plus' e1 e2)) xs) | x `isUnusedVar` e1 -> Plus' e1 (Min1' t2 (Map' t1 t2 (Lam x t e2) xs))
-  Min1' _ (Map' t1 t2 (Lam x t (Plus' e1 e2)) xs) | x `isUnusedVar` e2 -> Plus' (Min1' t2 (Map' t1 t2 (Lam x t e1) xs)) e1
-  ArgMax' _ (Map' _ _ (Lam x t e) xs) | x `isUnusedVar` e -> Minus' (Len' t xs) Lit1
-  ArgMax' _ (Map' t1 t2 (Lam x t (Plus' e1 e2)) xs) | x `isUnusedVar` e1 -> ArgMax' t2 (Map' t1 t2 (Lam x t e2) xs)
-  ArgMax' _ (Map' t1 t2 (Lam x t (Plus' e1 e2)) xs) | x `isUnusedVar` e2 -> ArgMax' t2 (Map' t1 t2 (Lam x t e1) xs)
-  ArgMin' _ (Map' _ _ (Lam x _ e) _) | x `isUnusedVar` e -> Lit0
-  ArgMin' _ (Map' t1 t2 (Lam x t (Plus' e1 e2)) xs) | x `isUnusedVar` e1 -> ArgMin' t2 (Map' t1 t2 (Lam x t e2) xs)
-  ArgMin' _ (Map' t1 t2 (Lam x t (Plus' e1 e2)) xs) | x `isUnusedVar` e2 -> ArgMin' t2 (Map' t1 t2 (Lam x t e1) xs)
-  e -> e
+  Len' _ (Map' t1 _ _ xs) -> Just $ Len' t1 xs
+  At' _ (Map' t1 _ f xs) i -> Just $ App f (At' t1 xs i)
+  Sum' (Map' t1 _ (Lam x _ e) xs) | x `isUnusedVar` e -> Just $ Mult' (Len' t1 xs) e
+  Sum' (Map' t1 t2 (Lam x t (Negate' e)) xs) -> Just $ Negate' (Sum' (Map' t1 t2 (Lam x t e) xs))
+  Sum' (Map' t1 t2 (Lam x t (Plus' e1 e2)) xs) -> Just $ Plus' (Sum' (Map' t1 t2 (Lam x t e1) xs)) (Sum' (Map' t1 t2 (Lam x t e2) xs))
+  Sum' (Map' t1 t2 (Lam x t (Mult' e1 e2)) xs) | x `isUnusedVar` e1 -> Just $ Mult' e1 (Sum' (Map' t1 t2 (Lam x t e2) xs))
+  Sum' (Map' t1 t2 (Lam x t (Mult' e1 e2)) xs) | x `isUnusedVar` e2 -> Just $ Mult' e2 (Sum' (Map' t1 t2 (Lam x t e1) xs))
+  Product' (Map' t1 _ (Lam x _ e) xs) | x `isUnusedVar` e -> Just $ Pow' e (Len' t1 xs)
+  Product' (Map' t1 t2 (Lam x t (Negate' e)) xs) -> Just $ Mult' (Pow' (Negate' Lit0) (Len' t1 xs)) (Product' (Map' t1 t2 (Lam x t e) xs))
+  Product' (Map' t1 t2 (Lam x t (Mult' e1 e2)) xs) -> Just $ Mult' (Product' (Map' t1 t2 (Lam x t e1) xs)) (Product' (Map' t1 t2 (Lam x t e2) xs))
+  Max1' _ (Map' _ _ (Lam x _ e) _) | x `isUnusedVar` e -> Just e
+  Max1' _ (Map' t1 t2 (Lam x t (Max2' t' e1 e2)) xs) -> Just $ Max2' t' (Map' t1 t2 (Lam x t e1) xs) (Map' t1 t2 (Lam x t e2) xs)
+  Max1' _ (Map' t1 t2 (Lam x t (Negate' e)) xs) -> Just $ Negate' (Min1' t2 (Map' t1 t2 (Lam x t e) xs))
+  Max1' _ (Map' t1 t2 (Lam x t (Plus' e1 e2)) xs) | x `isUnusedVar` e1 -> Just $ Plus' e1 (Max1' t2 (Map' t1 t2 (Lam x t e2) xs))
+  Max1' _ (Map' t1 t2 (Lam x t (Plus' e1 e2)) xs) | x `isUnusedVar` e2 -> Just $ Plus' (Max1' t2 (Map' t1 t2 (Lam x t e1) xs)) e1
+  Min1' _ (Map' _ _ (Lam x _ e) _) | x `isUnusedVar` e -> Just e
+  Min1' _ (Map' t1 t2 (Lam x t (Min2' t' e1 e2)) xs) -> Just $ Min2' t' (Map' t1 t2 (Lam x t e1) xs) (Map' t1 t2 (Lam x t e2) xs)
+  Min1' _ (Map' t1 t2 (Lam x t (Negate' e)) xs) -> Just $ Negate' (Max1' t2 (Map' t1 t2 (Lam x t e) xs))
+  Min1' _ (Map' t1 t2 (Lam x t (Plus' e1 e2)) xs) | x `isUnusedVar` e1 -> Just $ Plus' e1 (Min1' t2 (Map' t1 t2 (Lam x t e2) xs))
+  Min1' _ (Map' t1 t2 (Lam x t (Plus' e1 e2)) xs) | x `isUnusedVar` e2 -> Just $ Plus' (Min1' t2 (Map' t1 t2 (Lam x t e1) xs)) e1
+  ArgMax' _ (Map' _ _ (Lam x t e) xs) | x `isUnusedVar` e -> Just $ Minus' (Len' t xs) Lit1
+  ArgMax' _ (Map' t1 t2 (Lam x t (Plus' e1 e2)) xs) | x `isUnusedVar` e1 -> Just $ ArgMax' t2 (Map' t1 t2 (Lam x t e2) xs)
+  ArgMax' _ (Map' t1 t2 (Lam x t (Plus' e1 e2)) xs) | x `isUnusedVar` e2 -> Just $ ArgMax' t2 (Map' t1 t2 (Lam x t e1) xs)
+  ArgMin' _ (Map' _ _ (Lam x _ e) _) | x `isUnusedVar` e -> Just Lit0
+  ArgMin' _ (Map' t1 t2 (Lam x t (Plus' e1 e2)) xs) | x `isUnusedVar` e1 -> Just $ ArgMin' t2 (Map' t1 t2 (Lam x t e2) xs)
+  ArgMin' _ (Map' t1 t2 (Lam x t (Plus' e1 e2)) xs) | x `isUnusedVar` e2 -> Just $ ArgMin' t2 (Map' t1 t2 (Lam x t e1) xs)
+  _ -> Nothing
 
-reduceFoldBuild :: Expr -> Expr
-reduceFoldBuild = \case
-  Foldl' _ t (Lam2 x1 t1 x2 _ body) x (Range1' n) | x2 `isUnusedVar` body -> NatInd' t x (Lam x1 t1 body) n
-  Len' _ (Range1' n) -> n
-  At' _ (Range1' _) i -> i
-  Sum' (Range1' n) -> FloorDiv' (Mult' n (Minus' n Lit1)) Lit2
-  Sum' (Map' _ _ (Lam x _ (Mult' x' x'')) (Range1' n)) | x' == Var x && x'' == Var x -> FloorDiv' (Mult' n (Mult' (Minus' n Lit1) (Minus' (Mult' Lit2 n) Lit1))) (Lit (LitInt 6))
-  Sum' (Map' _ _ (Lam x _ (Mult' x' (Mult' x'' x'''))) (Range1' n)) | x' == Var x && x'' == Var x && x''' == Var x -> FloorDiv' (Mult' n (Mult' n (Mult' (Minus' n Lit1) (Minus' n Lit1)))) (Lit (LitInt 4))
-  Product' (Range1' n) -> If' IntTy (Equal' IntTy n Lit0) Lit1 Lit0
-  Max1' _ (Range1' n) -> Minus' n Lit1
-  Min1' _ (Range1' _) -> Lit0
-  ArgMax' _ (Range1' n) -> Minus' n Lit1
-  ArgMin' _ (Range1' _) -> Lit0
-  e -> e
+reduceFoldBuild :: Monad m => RewriteRule m
+reduceFoldBuild = simpleRewriteRule $ \case
+  Foldl' _ t (Lam2 x1 t1 x2 _ body) x (Range1' n) | x2 `isUnusedVar` body -> Just $ NatInd' t x (Lam x1 t1 body) n
+  Len' _ (Range1' n) -> Just n
+  At' _ (Range1' _) i -> Just i
+  Sum' (Range1' n) -> Just $ FloorDiv' (Mult' n (Minus' n Lit1)) Lit2
+  Sum' (Map' _ _ (Lam x _ (Mult' x' x'')) (Range1' n)) | x' == Var x && x'' == Var x -> Just $ FloorDiv' (Mult' n (Mult' (Minus' n Lit1) (Minus' (Mult' Lit2 n) Lit1))) (Lit (LitInt 6))
+  Sum' (Map' _ _ (Lam x _ (Mult' x' (Mult' x'' x'''))) (Range1' n)) | x' == Var x && x'' == Var x && x''' == Var x -> Just $ FloorDiv' (Mult' n (Mult' n (Mult' (Minus' n Lit1) (Minus' n Lit1)))) (Lit (LitInt 4))
+  Product' (Range1' n) -> Just $ If' IntTy (Equal' IntTy n Lit0) Lit1 Lit0
+  Max1' _ (Range1' n) -> Just $ Minus' n Lit1
+  Min1' _ (Range1' _) -> Just Lit0
+  ArgMax' _ (Range1' n) -> Just $ Minus' n Lit1
+  ArgMin' _ (Range1' _) -> Just Lit0
+  _ -> Nothing
 
-reduceFold :: Expr -> Expr
-reduceFold = \case
-  NatInd' _ v (Lam x _ (MatAp' n _ f (Var x'))) k | x `isUnusedVar` f && x == x' -> MatAp' n n (MatPow' n f k) v
-  e -> e
+reduceFold :: Monad m => RewriteRule m
+reduceFold = simpleRewriteRule $ \case
+  NatInd' _ v (Lam x _ (MatAp' n _ f (Var x'))) k | x `isUnusedVar` f && x == x' -> Just $ MatAp' n n (MatPow' n f k) v
+  _ -> Nothing
 
-reduceList :: MonadAlpha m => Expr -> m Expr
-reduceList e = do
-  e <- reduceBuild e
-  e <- reduceMapMap e
-  e <- return $ reduceFoldMap e
-  e <- return $ reduceFoldBuild e
-  return $ reduceFold e
+reduceList :: MonadAlpha m => RewriteRule m
+reduceList =
+  mconcat
+    [ reduceBuild,
+      reduceMapMap,
+      reduceFoldMap,
+      reduceFoldBuild,
+      reduceFold
+    ]
 
-misc :: Expr -> Expr
-misc = \case
+misc :: Monad m => RewriteRule m
+misc = simpleRewriteRule $ \case
   -- arithmetical functions
-  Pow' (Pow' e1 e2) e3 -> Pow' e1 (Plus' e2 e3)
+  Pow' (Pow' e1 e2) e3 -> Just $ Pow' e1 (Plus' e2 e3)
   -- advanced arithmetical functions
-  Gcd' (Mult' k1 e1) (Mult' k2 e2) | k1 == k2 -> Mult' k1 (Gcd' e1 e2)
-  Gcd' (Mult' k1 e1) (Mult' e2 k2) | k1 == k2 -> Mult' k1 (Gcd' e1 e2)
-  Gcd' (Mult' e1 k1) (Mult' e2 k2) | k1 == k2 -> Mult' k1 (Gcd' e1 e2)
-  Gcd' (Mult' e1 k1) (Mult' k2 e2) | k1 == k2 -> Mult' k1 (Gcd' e1 e2)
-  e -> e
+  Gcd' (Mult' k1 e1) (Mult' k2 e2) | k1 == k2 -> Just $ Mult' k1 (Gcd' e1 e2)
+  Gcd' (Mult' k1 e1) (Mult' e2 k2) | k1 == k2 -> Just $ Mult' k1 (Gcd' e1 e2)
+  Gcd' (Mult' e1 k1) (Mult' e2 k2) | k1 == k2 -> Just $ Mult' k1 (Gcd' e1 e2)
+  Gcd' (Mult' e1 k1) (Mult' k2 e2) | k1 == k2 -> Just $ Mult' k1 (Gcd' e1 e2)
+  _ -> Nothing
 
-runExpr :: MonadAlpha m => [(VarName, Type)] -> Expr -> m Expr
-runExpr _ e = do
-  e <- return $ eliminateSomeBuiltins e
-  e <- return $ reduceNegate e
-  e <- return $ reduceNot e
-  e <- return $ reduceBitNot e
-  e <- return $ reduceAssoc e
-  e <- reduceList e
-  return $ misc e
+rule :: MonadAlpha m => RewriteRule m
+rule =
+  mconcat
+    [ eliminateSomeBuiltins,
+      reduceNegate,
+      reduceNot,
+      reduceBitNot,
+      reduceAssoc,
+      reduceList,
+      misc
+    ]
 
 runProgram :: MonadAlpha m => Program -> m Program
-runProgram = mapExprProgramM' runExpr runExpr
+runProgram = applyRewriteRuleProgram' rule
 
 run :: (MonadAlpha m, MonadError Error m) => Program -> m Program
 run prog = wrapError' "Jikka.Core.Convert.StrengthReduction" $ do
   precondition $ do
     ensureWellTyped prog
-  prog <- runProgram prog
-  prog <- runProgram prog
-  prog <- runProgram prog
-  prog <- runProgram prog
   prog <- runProgram prog
   postcondition $ do
     ensureWellTyped prog
---
+++ src/Jikka/Core/Language/RewriteRules.hs
@@ -0,0 +1,105 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE LambdaCase #-}
+
+module Jikka.Core.Language.RewriteRules
+  ( RewriteRule (..),
+    pureRewriteRule,
+    simpleRewriteRule,
+    applyRewriteRule,
+    applyRewriteRuleToplevelExpr,
+    applyRewriteRuleProgram,
+    applyRewriteRuleProgram',
+  )
+where
+
+import Data.Maybe (fromMaybe)
+import Jikka.Core.Language.Expr
+import Jikka.Core.Language.Util (curryFunTy)
+
+newtype RewriteRule m = RewriteRule ([(VarName, Type)] -> Expr -> m (Maybe Expr))
+
+unRewriteRule :: RewriteRule m -> [(VarName, Type)] -> Expr -> m (Maybe Expr)
+unRewriteRule (RewriteRule f) = f
+
+instance Monad m => Semigroup (RewriteRule m) where
+  f <> g = RewriteRule $ \env e -> do
+    e' <- unRewriteRule f env e
+    case e' of
+      Nothing -> unRewriteRule g env e
+      Just e' -> do
+        e'' <- unRewriteRule g env e'
+        case e'' of
+          Nothing -> return (Just e')
+          Just e'' -> return (Just e'')
+
+instance Monad m => Monoid (RewriteRule m) where
+  mempty = RewriteRule (\_ _ -> return Nothing)
+
+pureRewriteRule :: Monad m => ([(VarName, Type)] -> Expr -> Maybe Expr) -> RewriteRule m
+pureRewriteRule f = RewriteRule (\env e -> return (f env e))
+
+simpleRewriteRule :: Monad m => (Expr -> Maybe Expr) -> RewriteRule m
+simpleRewriteRule f = RewriteRule (\_ e -> return (f e))
+
+-- | `applyRewriteRule` applies a given rule to a given expr.
+-- This rewrites on all sub-exprs of the given expr, and repeats to rewrite while it is possible.
+applyRewriteRule :: Monad m => RewriteRule m -> [(VarName, Type)] -> Expr -> m (Maybe Expr)
+applyRewriteRule = applyRewriteRulePreOrder
+
+coalesceMaybes :: a -> Maybe a -> b -> Maybe b -> Maybe (a, b)
+coalesceMaybes _ Nothing _ Nothing = Nothing
+coalesceMaybes a Nothing _ (Just b) = Just (a, b)
+coalesceMaybes _ (Just a) b Nothing = Just (a, b)
+coalesceMaybes _ (Just a) _ (Just b) = Just (a, b)
+
+applyRewriteRuleToImmediateSubExprs :: Monad m => RewriteRule m -> [(VarName, Type)] -> Expr -> m (Maybe Expr)
+applyRewriteRuleToImmediateSubExprs f env = \case
+  Var _ -> return Nothing
+  Lit _ -> return Nothing
+  App e1 e2 -> do
+    e1' <- unRewriteRule f env e1
+    e2' <- unRewriteRule f env e2
+    return $ fmap (uncurry App) (coalesceMaybes e1 e1' e2 e2')
+  Lam x t body -> (Lam x t <$>) <$> unRewriteRule f ((x, t) : env) body
+  Let x t e1 e2 -> do
+    e1' <- unRewriteRule f env e1
+    e2' <- unRewriteRule f ((x, t) : env) e2
+    return $ fmap (uncurry (Let x t)) (coalesceMaybes e1 e1' e2 e2')
+
+applyRewriteRulePreOrder :: Monad m => RewriteRule m -> [(VarName, Type)] -> Expr -> m (Maybe Expr)
+applyRewriteRulePreOrder f env e = do
+  e' <- unRewriteRule f env e
+  case e' of
+    Nothing -> do
+      e' <- applyRewriteRuleToImmediateSubExprs (RewriteRule (applyRewriteRulePreOrder f)) env e
+      case e' of
+        Nothing -> return Nothing
+        Just e' -> do
+          e'' <- unRewriteRule f env e'
+          case e'' of
+            Nothing -> return $ Just e'
+            Just e'' -> do
+              e''' <- applyRewriteRulePreOrder f env e''
+              return . Just $ fromMaybe e'' e'''
+    Just e' -> do
+      e'' <- applyRewriteRulePreOrder f env e'
+      return . Just $ fromMaybe e' e''
+
+applyRewriteRuleToplevelExpr :: Monad m => RewriteRule m -> [(VarName, Type)] -> ToplevelExpr -> m (Maybe ToplevelExpr)
+applyRewriteRuleToplevelExpr f env = \case
+  ResultExpr e -> (ResultExpr <$>) <$> applyRewriteRule f env e
+  ToplevelLet y t e cont -> do
+    e' <- applyRewriteRule f env e
+    cont' <- applyRewriteRuleToplevelExpr f ((y, t) : env) cont
+    return $ fmap (uncurry (ToplevelLet y t)) (coalesceMaybes e e' cont cont')
+  ToplevelLetRec g args ret body cont -> do
+    let env' = (g, curryFunTy (map snd args) ret) : env
+    body' <- applyRewriteRule f (reverse args ++ env') body
+    cont' <- applyRewriteRuleToplevelExpr f env' cont
+    return $ fmap (uncurry (ToplevelLetRec g args ret)) (coalesceMaybes body body' cont cont')
+
+applyRewriteRuleProgram :: Monad m => RewriteRule m -> Program -> m (Maybe Program)
+applyRewriteRuleProgram f = applyRewriteRuleToplevelExpr f []
+
+applyRewriteRuleProgram' :: Monad m => RewriteRule m -> Program -> m Program
+applyRewriteRuleProgram' f prog = fromMaybe prog <$> applyRewriteRuleProgram f prog
