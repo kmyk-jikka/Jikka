9176e426038ffcbc5a12facea9f06c7e28e8cfeb
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-07-07 21:37:24 +0900

    refactor(core): Update Jikka.Core.Convert.CloseSum a little

--- src/Jikka/Core/Convert/CloseSum.hs
+++ src/Jikka/Core/Convert/CloseSum.hs
@@ -14,6 +14,10 @@ module Jikka.Core.Convert.CloseSum
 
     -- * internal rules
     rule,
+    reduceSum,
+    reduceProduct,
+    reduceModSum,
+    reduceModProduct,
   )
 where
 
@@ -25,28 +29,74 @@ import Jikka.Core.Language.FreeVars
 import Jikka.Core.Language.Lint
 import Jikka.Core.Language.RewriteRules
 
-rule :: Monad m => RewriteRule m
-rule = simpleRewriteRule $ \case
-  -- reduce `Reversed`
+reduceSum :: Monad m => RewriteRule m
+reduceSum = simpleRewriteRule $ \case
+  -- reduce list build functions
+  Sum' (Nil' _) -> Just Lit0
+  Sum' (Cons' _ x xs) -> Just $ Plus' x (Sum' xs)
+  Sum' (Range1' n) -> Just $ FloorDiv' (Mult' n (Plus' n Lit1)) Lit2
+  -- reduce list map functions
   Sum' (Reversed' _ xs) -> Just $ Sum' xs
-  Product' (Reversed' _ xs) -> Just $ Product' xs
-  ModSum' (Reversed' _ xs) m -> Just $ ModSum' xs m
-  ModProduct' (Reversed' _ xs) m -> Just $ ModProduct' xs m
-  -- reduce `Sorted`
   Sum' (Sorted' _ xs) -> Just $ Sum' xs
-  Product' (Sorted' _ xs) -> Just $ Product' xs
-  ModSum' (Sorted' _ xs) m -> Just $ ModSum' xs m
-  ModProduct' (Sorted' _ xs) m -> Just $ ModProduct' xs m
-  -- reduce `Map`
   Sum' (Map' t1 _ (Lam x _ e) xs) | x `isUnusedVar` e -> Just $ Mult' (Len' t1 xs) e
   Sum' (Map' t1 t2 (Lam x t (Negate' e)) xs) -> Just $ Negate' (Sum' (Map' t1 t2 (Lam x t e) xs))
   Sum' (Map' t1 t2 (Lam x t (Plus' e1 e2)) xs) -> Just $ Plus' (Sum' (Map' t1 t2 (Lam x t e1) xs)) (Sum' (Map' t1 t2 (Lam x t e2) xs))
   Sum' (Map' t1 t2 (Lam x t (Mult' e1 e2)) xs) | x `isUnusedVar` e1 -> Just $ Mult' e1 (Sum' (Map' t1 t2 (Lam x t e2) xs))
   Sum' (Map' t1 t2 (Lam x t (Mult' e1 e2)) xs) | x `isUnusedVar` e2 -> Just $ Mult' e2 (Sum' (Map' t1 t2 (Lam x t e1) xs))
+  -- others
+  _ -> Nothing
+
+reduceProduct :: Monad m => RewriteRule m
+reduceProduct = simpleRewriteRule $ \case
+  -- reduce list build functions
+  Product' (Nil' _) -> Just Lit1
+  Product' (Cons' _ x xs) -> Just $ Mult' x (Product' xs)
+  Product' (Range1' n) -> Just $ If' IntTy (Equal' IntTy n Lit0) Lit1 Lit0
+  -- reduce list map functions
+  Product' (Reversed' _ xs) -> Just $ Product' xs
+  Product' (Sorted' _ xs) -> Just $ Product' xs
   Product' (Map' t1 _ (Lam x _ e) xs) | x `isUnusedVar` e -> Just $ Pow' e (Len' t1 xs)
   Product' (Map' t1 t2 (Lam x t (Negate' e)) xs) -> Just $ Mult' (Pow' (Negate' Lit0) (Len' t1 xs)) (Product' (Map' t1 t2 (Lam x t e) xs))
   Product' (Map' t1 t2 (Lam x t (Mult' e1 e2)) xs) -> Just $ Mult' (Product' (Map' t1 t2 (Lam x t e1) xs)) (Product' (Map' t1 t2 (Lam x t e2) xs))
+  -- others
   _ -> Nothing
+
+reduceModSum :: Monad m => RewriteRule m
+reduceModSum = simpleRewriteRule $ \case
+  -- the corner case
+  ModSum' _ Lit1 -> Just Lit0
+  -- reduce list build functions
+  ModSum' (Nil' _) _ -> Just Lit0
+  ModSum' (Cons' _ x xs) m -> Just $ ModPlus' x (ModSum' xs m) m
+  ModSum' (Range1' n) m -> Just $ FloorMod' (FloorDiv' (Mult' n (Plus' n Lit1)) Lit2) m
+  -- reduce list map functions
+  ModSum' (Reversed' _ xs) m -> Just $ ModSum' xs m
+  ModSum' (Sorted' _ xs) m -> Just $ ModSum' xs m
+  -- others
+  _ -> Nothing
+
+reduceModProduct :: Monad m => RewriteRule m
+reduceModProduct = simpleRewriteRule $ \case
+  -- the corner case
+  ModProduct' _ Lit1 -> Just Lit0
+  -- reduce list build functions
+  ModProduct' (Nil' _) m -> Just $ FloorMod' Lit1 m
+  ModProduct' (Cons' _ x xs) m -> Just $ ModMult' x (ModProduct' xs m) m
+  ModProduct' (Range1' n) m -> Just $ If' IntTy (Equal' IntTy n Lit0) (FloorMod' Lit1 m) Lit0
+  -- reduce list map functions
+  ModProduct' (Reversed' _ xs) m -> Just $ ModProduct' xs m
+  ModProduct' (Sorted' _ xs) m -> Just $ ModProduct' xs m
+  -- others
+  _ -> Nothing
+
+rule :: MonadAlpha m => RewriteRule m
+rule =
+  mconcat
+    [ reduceSum,
+      reduceProduct,
+      reduceModSum,
+      reduceModProduct
+    ]
 
 runProgram :: MonadAlpha m => Program -> m Program
 runProgram = applyRewriteRuleProgram' rule
