a866039c12bb20bdb6f0d979d2eb4f9c79b6a078
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-07-09 23:17:21 +0900

    fix(rpython): Convert splices from restricted Python to core

--- src/Jikka/RestrictedPython/Convert/ToCore.hs
+++ src/Jikka/RestrictedPython/Convert/ToCore.hs
@@ -250,7 +250,23 @@ runExpr e0 = maybe id wrapAt (loc' e0) $ case value' e0 of
     let t' = runType t
     foldr (Y.Cons' t') (Y.Lit (Y.LitNil t')) <$> mapM runExpr es
   X.Tuple es -> Y.uncurryApp <$> (Y.Tuple' <$> mapM (const Y.genType) es) <*> mapM runExpr es
-  X.SubscriptSlice _ _ _ _ -> throwInternalError "runExpr TODO"
+  X.SubscriptSlice e from to step -> do
+    e <- runExpr e
+    from <- traverse runExpr from
+    to <- traverse runExpr to
+    step <- traverse runExpr step
+    i <- Y.genVarName'
+    t <- Y.genType
+    let mapAt = return . Y.Map' Y.IntTy t (Y.Lam i t (Y.At' t e (Y.Var i)))
+    case (from, to, step) of
+      (Nothing, Nothing, Nothing) -> return e
+      (Nothing, Just to, Nothing) -> mapAt (Y.Range1' to)
+      (Just from, Nothing, Nothing) -> mapAt (Y.Range2' from (Y.Len' t e))
+      (Just from, Just to, Nothing) -> mapAt (Y.Range2' from to)
+      (Nothing, Nothing, Just step) -> mapAt (Y.Range3' Y.Lit0 (Y.Len' t e) step)
+      (Nothing, Just to, Just step) -> mapAt (Y.Range3' Y.Lit0 to step)
+      (Just from, Nothing, Just step) -> mapAt (Y.Range3' from (Y.Len' t e) step)
+      (Just from, Just to, Just step) -> mapAt (Y.Range3' from to step)
 
 -- | `runForStatement` converts for-loops to `foldl`.
 -- For example, this converts the following:
