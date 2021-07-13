8c045b24f6d0a6a94c16a6394c784c964cd093e3
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-07-13 23:11:03 +0900

    refactor(rpython): Update Jikka.RestrictedPython.Convert.ToCore.runAttribute

--- src/Jikka/RestrictedPython/Convert/ToCore.hs
+++ src/Jikka/RestrictedPython/Convert/ToCore.hs
@@ -149,21 +149,17 @@ runBuiltin builtin =
         X.BuiltinMultiChoose -> f Y.MultiChoose
         X.BuiltinModInv -> f Y.ModInv
 
-runAttribute :: (MonadAlpha m, MonadError Error m) => Y.Expr -> X.Attribute' -> m Y.Expr
-runAttribute e a = maybe id wrapAt (loc' a) $ do
+runAttribute :: MonadError Error m => X.Attribute' -> m Y.Expr
+runAttribute a = maybe id wrapAt (loc' a) $ do
   case value' a of
     X.UnresolvedAttribute a -> throwInternalError $ "unresolved attribute: " ++ X.unAttributeName a
     X.BuiltinCount t -> do
-      x <- Y.genVarName'
-      y <- Y.genVarName'
       let t' = runType t
-      return $ Y.Lam x t' (Y.Len' t' (Y.Filter' t' (Y.Lam y t' (Y.Equal' t' (Y.Var x) (Y.Var y))) e))
+      return $ Y.Lam2 "xs" (Y.ListTy t') "x" t' (Y.Len' t' (Y.Filter' t' (Y.Lam "y" t' (Y.Equal' t' (Y.Var "x") (Y.Var "y"))) (Y.Var "xs")))
     X.BuiltinIndex t -> do
-      x <- Y.genVarName'
-      i <- Y.genVarName'
       let t' = runType t
-      return $ Y.Lam x t' (Y.Min1' Y.IntTy (Y.Filter' Y.IntTy (Y.Lam i Y.IntTy (Y.Equal' t' (Y.At' t' e (Y.Var i)) (Y.Var x))) (Y.Range1' (Y.Len' t' e))))
-    X.BuiltinCopy _ -> return e
+      return $ Y.Lam2 "xs" (Y.ListTy t') "x" t' (Y.Min1' Y.IntTy (Y.Filter' Y.IntTy (Y.Lam "i" Y.IntTy (Y.Equal' t' (Y.At' t' (Y.Var "xs") (Y.Var "i")) (Y.Var "x"))) (Y.Range1' (Y.Len' t' (Y.Var "xs")))))
+    X.BuiltinCopy t -> return $ Y.Lam "x" (runType t) (Y.Var "x")
 
 runBoolOp :: X.BoolOp -> Y.Builtin
 runBoolOp = \case
@@ -262,7 +258,8 @@ runExpr e0 = maybe id wrapAt (loc' e0) $ case value' e0 of
   X.Constant const -> runConstant const
   X.Attribute e a -> do
     e <- runExpr e
-    runAttribute e a
+    a <- runAttribute a
+    return $ Y.App a e
   X.Subscript e1 e2 -> Y.AppBuiltin2 <$> (Y.At <$> Y.genType) <*> runExpr e1 <*> runExpr e2
   X.Name x -> return $ Y.Var (runVarName x)
   X.List t es -> do
