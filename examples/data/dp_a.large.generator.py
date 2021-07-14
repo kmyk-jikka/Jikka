e6c689c6cf1a0c0f4e5761d9ac53eea869d48858
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-07-14 19:43:01 +0900

    test: Now examples/dp_a.py can get AC

--- examples/data/dp_a.large.generator.py
+++ examples/data/dp_a.large.generator.py
@@ -2,8 +2,7 @@
 import random
 
 def main() -> None:
-    # n = random.randint(2, 10 ** 5)
-    n = random.randint(2, 10 ** 3)
+    n = random.randint(2, 10 ** 5)
     h = [random.randint(1, 10 ** 4) for _ in range(n)]
     print(n)
     print(*h)
