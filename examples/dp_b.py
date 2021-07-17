5ba7bf982f020d4267834586faf6c4a21fe448c4
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-07-18 04:33:36 +0900

    test: Use dp_b.py and dp_z.py for tests

--- examples/dp_b.py
+++ examples/dp_b.py
@@ -1,6 +1,7 @@
 # https://atcoder.jp/contests/dp/tasks/dp_b
-import math
 from typing import *
+
+INF = 10 ** 18
 
 def solve(n: int, k: int, h: List[int]) -> int:
     assert 2 <= n <= 10 ** 5
@@ -8,9 +9,19 @@ def solve(n: int, k: int, h: List[int]) -> int:
     assert len(h) == n
     assert all(1 <= h_i <= 10 ** 4 for h_i in h)
 
-    dp = [math.inf for _ in range(n)]
+    dp = [INF for _ in range(n)]
     dp[0] = 0
     for i in range(1, n):
         for j in range(max(0, i - k), i):
             dp[i] = min(dp[i], dp[j] + abs(h[i] - h[j]))
     return dp[n - 1]
+
+def main() -> None:
+    n, k = map(int, input().split())
+    h = list(map(int, input().split()))
+    assert len(h) == n
+    ans = solve(n, k, h)
+    print(ans)
+
+if __name__ == '__main__':
+    main()
