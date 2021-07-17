b16f97b095dde03fea4179310680ec9101055b0f
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-07-18 06:05:16 +0900

    feat(rpython): Allow `xs = list(range(q))` in main functions

--- examples/static_range_sum.py
+++ examples/static_range_sum.py
@@ -7,3 +7,18 @@ def solve(n: int, q: int, a: List[int], l: List[int], r: List[int]) -> List[int]
     for i in range(q):
         ans[i] = sum(a[l[i]:r[i]])
     return ans
+
+def main() -> None:
+    n, q = map(int, input().split())
+    a = list(map(int, input().split()))
+    assert len(a) == n
+    l = list(range(q))
+    r = list(range(q))
+    for i in range(q):
+        l[i], r[i] = map(int, input().split())
+    ans = solve(n, q, a, l, r)
+    for i in range(q):
+        print(ans[i])
+
+if __name__ == '__main__':
+    main()
