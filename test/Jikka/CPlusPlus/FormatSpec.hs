741f68970518c32f1f4d0ce80fa2593ffeca4ccf
uta8a <noumodulity@gmail.com>
2021-08-05 17:48:31 +0900

    fix(cxx): add test (no unnecessary paren in index)

--- test/Jikka/CPlusPlus/FormatSpec.hs
+++ test/Jikka/CPlusPlus/FormatSpec.hs
@@ -12,33 +12,64 @@ run'' :: Program -> [String]
 run'' prog = dropWhile ("#include" `isPrefixOf`) (lines (run' prog))
 
 spec :: Spec
-spec = describe "run" $ do
-  it "works" $ do
-    let program =
-          Program
-            [ FunDef
-                TyInt64
-                (VarName "solve")
-                [(TyInt32, VarName "n")]
-                [ Declare TyInt64 (VarName "x") (DeclareCopy (Lit (LitInt64 0))),
-                  For
-                    TyInt32
-                    (VarName "i")
-                    (Lit (LitInt32 0))
-                    (BinOp LessThan (Var (VarName "i")) (Var (VarName "n")))
-                    (AssignIncr (LeftVar (VarName "i")))
-                    [ Assign (AssignExpr AddAssign (LeftVar (VarName "x")) (Call (Cast TyInt64) [Var (VarName "i")]))
-                    ],
-                  Return (Var (VarName "x"))
-                ]
+spec = do
+  describe "run" $ do
+    it "works" $ do
+      let program =
+            Program
+              [ FunDef
+                  TyInt64
+                  (VarName "solve")
+                  [(TyInt32, VarName "n")]
+                  [ Declare TyInt64 (VarName "x") (DeclareCopy (Lit (LitInt64 0))),
+                    For
+                      TyInt32
+                      (VarName "i")
+                      (Lit (LitInt32 0))
+                      (BinOp LessThan (Var (VarName "i")) (Var (VarName "n")))
+                      (AssignIncr (LeftVar (VarName "i")))
+                      [ Assign (AssignExpr AddAssign (LeftVar (VarName "x")) (Call (Cast TyInt64) [Var (VarName "i")]))
+                      ],
+                    Return (Var (VarName "x"))
+                  ]
+              ]
+      let formatted =
+            [ "int64_t solve(int32_t n) {",
+              "    int64_t x = 0;",
+              "    for (int32_t i = 0; i < n; ++ i) {",
+              "        x += int64_t(i);",
+              "    }",
+              "    return x;",
+              "}"
             ]
-    let formatted =
-          [ "int64_t solve(int32_t n) {",
-            "    int64_t x = 0;",
-            "    for (int32_t i = 0; i < n; ++ i) {",
-            "        x += int64_t(i);",
-            "    }",
-            "    return x;",
-            "}"
-          ]
-    run'' program `shouldBe` formatted
+      run'' program `shouldBe` formatted
+  describe "no unnecessary paren in index" $ do
+    it "works" $ do
+      let program =
+            Program
+              [ FunDef
+                  TyInt64
+                  (VarName "solve")
+                  [(TyInt32, VarName "n"), (TyVector TyInt64, VarName "h")]
+                  [ Declare TyInt64 (VarName "x") (DeclareCopy (Lit (LitInt64 0))),
+                    For
+                      TyInt32
+                      (VarName "i")
+                      (Lit (LitInt32 2))
+                      (BinOp LessThan (Var (VarName "i")) (Var (VarName "n")))
+                      (AssignIncr (LeftVar (VarName "i")))
+                      [ Assign (AssignExpr AddAssign (LeftVar (VarName "x")) (Call At [Var (VarName "h"), BinOp Sub (Var (VarName "i")) (Lit (LitInt32 2))]))
+                      ],
+                    Return (Var (VarName "x"))
+                  ]
+              ]
+      let formatted =
+            [ "int64_t solve(int32_t n, std::vector<int64_t> h) {",
+              "    int64_t x = 0;",
+              "    for (int32_t i = 2; i < n; ++ i) {",
+              "        x += h[i - 2];",
+              "    }",
+              "    return x;",
+              "}"
+            ]
+      run'' program `shouldBe` formatted
