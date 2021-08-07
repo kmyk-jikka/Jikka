b45e3844c3ae292b250693b0df7cc05cbd9d9022
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-08-07 10:17:27 +0900

    test(cxx): Add tests to MoveSemantics

--- test/Jikka/CPlusPlus/Convert/MoveSemanticsSpec.hs
+++ test/Jikka/CPlusPlus/Convert/MoveSemanticsSpec.hs
@@ -71,3 +71,104 @@ spec = describe "run" $ do
                 ]
             ]
     run' prog `shouldBe` Right expected
+
+  it "don't move if it is used in the next loop iteration" $ do
+    let prog =
+          Program
+            [ FunDef
+                TyInt32
+                "func"
+                [(TyInt32, "a")]
+                [ For
+                    TyInt32
+                    "i"
+                    (Lit (LitInt32 0))
+                    (BinOp LessThan (Var "i") (Lit (LitInt32 10)))
+                    (AssignIncr (LeftVar "i"))
+                    [ Declare TyInt32 "b" (DeclareCopy (Var "a")),
+                      Assign (AssignExpr AddAssign (LeftVar "b") (Lit (LitInt32 10)))
+                    ],
+                  Return (Var "a")
+                ]
+            ]
+    let expected = prog
+    run' prog `shouldBe` Right expected
+
+  it "allows to write back" $ do
+    let prog =
+          Program
+            [ FunDef
+                TyInt32
+                "func"
+                [(TyInt32, "a")]
+                [ Declare TyInt32 "b" (DeclareCopy (Var "a")),
+                  Assign (AssignExpr AddAssign (LeftVar "b") (Lit (LitInt32 10))),
+                  Assign (AssignExpr SimpleAssign (LeftVar "a") (Var "b")),
+                  Return (Var "a")
+                ]
+            ]
+    let expected =
+          Program
+            [ FunDef
+                TyInt32
+                "func"
+                [(TyInt32, "a")]
+                [ Assign (AssignExpr AddAssign (LeftVar "a") (Lit (LitInt32 10))),
+                  Return (Var "a")
+                ]
+            ]
+    run' prog `shouldBe` Right expected
+
+  it "TODO: distinguish indices" $ do
+    let prog =
+          Program
+            [ FunDef
+                (TyVector TyInt32)
+                "func"
+                [(TyVector TyInt32, "a")]
+                [ Declare (TyVector TyInt32) "b" (DeclareCopy (Var "a")),
+                  Assign (AssignExpr AddAssign (LeftAt (LeftVar "b") (Lit (LitInt32 0))) (Call At [Var "a", Lit (LitInt32 1)])),
+                  Assign (AssignExpr SimpleAssign (LeftVar "a") (Var "b")),
+                  Return (Var "a")
+                ]
+            ]
+    let expected =
+          Program
+            [ FunDef
+                (TyVector TyInt32)
+                "func"
+                [(TyVector TyInt32, "a")]
+                [ Assign (AssignExpr AddAssign (LeftAt (LeftVar "a") (Lit (LitInt32 0))) (Call At [Var "a", Lit (LitInt32 1)])),
+                  Return (Var "a")
+                ]
+            ]
+    -- TODO: fix this
+    -- run' prog `shouldBe` Right expected
+    run' prog `shouldNotBe` Right expected
+
+  it "TODO: allows to write back and reads after it" $ do
+    let prog =
+          Program
+            [ FunDef
+                TyInt32
+                "func"
+                [(TyInt32, "a")]
+                [ Declare TyInt32 "b" (DeclareCopy (Var "a")),
+                  Assign (AssignExpr AddAssign (LeftVar "b") (Lit (LitInt32 10))),
+                  Assign (AssignExpr AddAssign (LeftVar "a") (Var "b")),
+                  Return (BinOp Add (Var "a") (Var "b"))
+                ]
+            ]
+    let expected =
+          Program
+            [ FunDef
+                TyInt32
+                "func"
+                [(TyInt32, "a")]
+                [ Assign (AssignExpr AddAssign (LeftVar "a") (Lit (LitInt32 10))),
+                  Return (BinOp Add (Var "a") (Var "a"))
+                ]
+            ]
+    -- TODO: fix this
+    -- run' prog `shouldBe` Right expected
+    run' prog `shouldNotBe` Right expected
