337906e521838795b60f0522aac356b00a30c99e
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-08-24 06:48:04 +0900

    feat(core): Update PropagateMod

--- src/Jikka/Core/Language/ModuloExpr.hs
+++ src/Jikka/Core/Language/ModuloExpr.hs
@@ -16,6 +16,11 @@ module Jikka.Core.Language.ModuloExpr
     isOneModuloExpr,
     moduloOfModuloExpr,
     arithmeticExprFromModuloExpr,
+
+    -- * Utilities
+    Modulo (..),
+    formatModulo,
+    isModulo,
   )
 where
 
@@ -25,17 +30,51 @@ import Jikka.Core.Format (formatExpr)
 import Jikka.Core.Language.ArithmeticExpr
 import Jikka.Core.Language.BuiltinPatterns
 import Jikka.Core.Language.Expr
-import Jikka.Core.Language.Runtime (modpow)
+import Jikka.Core.Language.Runtime (modinv, modpow)
+import Jikka.Core.Language.Util
 
+-- | `Modulo` is just a newtype to avoid mistakes that swapping left and right of mod-op.
 newtype Modulo = Modulo {unModulo :: Expr}
   deriving (Eq, Ord, Show, Read)
 
 formatModulo :: Modulo -> String
 formatModulo = formatExpr . unModulo
 
+isModulo :: Expr -> Modulo -> Bool
+isModulo e (Modulo m) = case e of
+  FloorMod' _ m' -> m' == m
+  ModNegate' _ m' -> m' == m
+  ModPlus' _ _ m' -> m' == m
+  ModMinus' _ _ m' -> m' == m
+  ModMult' _ _ m' -> m' == m
+  ModInv' _ m' -> m' == m
+  ModPow' _ _ m' -> m' == m
+  VecFloorMod' _ _ m' -> m' == m
+  MatFloorMod' _ _ _ m' -> m' == m
+  ModMatAp' _ _ _ _ m' -> m' == m
+  ModMatAdd' _ _ _ _ m' -> m' == m
+  ModMatMul' _ _ _ _ _ m' -> m' == m
+  ModMatPow' _ _ _ m' -> m' == m
+  ModSum' _ m' -> m' == m
+  ModProduct' _ m' -> m' == m
+  LitInt' n -> case m of
+    LitInt' m -> 0 <= n && n < m
+    _ -> False
+  Proj' ts _ e | isVectorTy' ts -> e `isModulo` Modulo m
+  Proj' ts _ e | isMatrixTy' ts -> e `isModulo` Modulo m
+  Map' _ _ f _ -> f `isModulo` Modulo m
+  Lam _ _ body -> body `isModulo` Modulo m
+  e@(App _ _) -> case curryApp e of
+    (e@(Lam _ _ _), _) -> e `isModulo` Modulo m
+    (Tuple' ts, es) | isVectorTy' ts -> all (`isModulo` Modulo m) es
+    (Tuple' ts, es) | isMatrixTy' ts -> all (`isModulo` Modulo m) es
+    _ -> False
+  _ -> False
+
 data ProductExpr = ProductExpr
   { productExprConst :: Integer,
-    productExprList :: [Expr]
+    productExprList :: [Expr],
+    productExprInvList :: [Expr]
   }
   deriving (Eq, Ord, Show, Read)
 
@@ -64,15 +103,50 @@ multProductExpr :: ProductExpr -> ProductExpr -> ProductExpr
 multProductExpr e1 e2 =
   ProductExpr
     { productExprConst = productExprConst e1 * productExprConst e2,
-      productExprList = productExprList e1 ++ productExprList e2
+      productExprList = productExprList e1 ++ productExprList e2,
+      productExprInvList = productExprInvList e1 ++ productExprInvList e2
     }
 
 powProductExpr :: Integer -> ProductExpr -> Integer -> ProductExpr
 powProductExpr m e k =
   ProductExpr
     { productExprConst = fromSuccess $ modpow (productExprConst e) k m,
-      productExprList = map (\e -> ModPow' e (LitInt' k) (LitInt' m)) (productExprList e)
+      productExprList = map (\e -> ModPow' e (LitInt' k) (LitInt' m)) (productExprList e),
+      productExprInvList = map (\e -> ModPow' e (LitInt' k) (LitInt' m)) (productExprInvList e)
     }
+
+partitionMaybe :: (a -> Maybe b) -> [a] -> ([b], [a])
+partitionMaybe f = \case
+  [] -> ([], [])
+  x : xs ->
+    let (ys, xs') = partitionMaybe f xs
+     in case f x of
+          Just y -> (y : ys, xs')
+          Nothing -> (ys, x : xs')
+
+fromLitInt :: Expr -> Maybe Integer
+fromLitInt (LitInt' k) = Just k
+fromLitInt _ = Nothing
+
+invProductExpr :: Modulo -> ProductExpr -> ProductExpr
+invProductExpr m e =
+  let (invKs, invList) = partitionMaybe fromLitInt (productExprInvList e)
+      e' =
+        ProductExpr
+          { productExprConst = product invKs,
+            productExprList = LitInt' (productExprConst e) : invList,
+            productExprInvList = productExprList e
+          }
+   in case m of
+        Modulo (LitInt' m) -> case modinv (productExprConst e) m of
+          Right k ->
+            ProductExpr
+              { productExprConst = (k * product invKs) `mod` m,
+                productExprList = invList,
+                productExprInvList = productExprList e
+              }
+          _ -> e'
+        _ -> e'
 
 isInteger :: Modulo -> Bool
 isInteger (Modulo (LitInt' _)) = True
@@ -84,14 +158,16 @@ moduloToInteger m = error $ "Jikka.Core.Language.ModuloExpr.moduloToInteger: mod
 
 parseProductExpr :: Modulo -> Expr -> ProductExpr
 parseProductExpr m = \case
-  LitInt' n -> ProductExpr {productExprConst = n, productExprList = []}
+  LitInt' n -> ProductExpr {productExprConst = n, productExprList = [], productExprInvList = []}
   Negate' e -> negateProductExpr (parseProductExpr m e)
   Mult' e1 e2 -> multProductExpr (parseProductExpr m e1) (parseProductExpr m e2)
+  JustDiv' e1 e2 -> multProductExpr (parseProductExpr m e1) (invProductExpr m (parseProductExpr m e2))
   Pow' e1 (LitInt' k) | isInteger m -> powProductExpr (moduloToInteger m) (parseProductExpr m e1) k
   ModNegate' e m' | Modulo m' == m -> negateProductExpr (parseProductExpr m e)
   ModMult' e1 e2 m' | Modulo m' == m -> multProductExpr (parseProductExpr m e1) (parseProductExpr m e2)
   ModPow' e1 (LitInt' k) (LitInt' m') | Modulo (LitInt' m') == m -> powProductExpr m' (parseProductExpr m e1) k
-  e -> ProductExpr {productExprConst = 1, productExprList = [e]}
+  ModInv' e1 m' | Modulo m' == m -> invProductExpr m (parseProductExpr m e1)
+  e -> ProductExpr {productExprConst = 1, productExprList = [e], productExprInvList = []}
 
 sumExprFromProductExpr :: ProductExpr -> SumExpr
 sumExprFromProductExpr e =
@@ -179,40 +255,73 @@ parseModuloExpr e = do
   m <- getModuloFromExpr e
   return $ ModuloExpr (parseSumExpr m e) m
 
-formatProductExpr :: ProductExpr -> Expr
-formatProductExpr e =
+formatTopProductExpr :: Modulo -> ProductExpr -> Expr
+formatTopProductExpr m e =
   let k = LitInt' (productExprConst e)
       k' e' = case productExprConst e of
         0 -> LitInt' 0
         1 -> e'
         -1 -> Negate' e'
         _ -> Mult' e' k
-   in case productExprList e of
+      invList = map (`FloorMod'` unModulo m) (productExprInvList e)
+   in case productExprList e ++ invList of
         [] -> k
         eHead : esTail -> k' (foldl Mult' eHead esTail)
 
-formatSumExpr :: SumExpr -> Expr
-formatSumExpr e = case sumExprList e of
+formatTopSumExpr :: Modulo -> SumExpr -> Expr
+formatTopSumExpr m e = case sumExprList e of
   [] -> LitInt' (sumExprConst e)
   eHead : esTail ->
     let op e'
           | productExprConst e' > 0 = Plus'
           | productExprConst e' < 0 = Minus'
           | otherwise = const
-        go e1 e2 = op e2 e1 (formatProductExpr (e2 {productExprConst = abs (productExprConst e2)}))
+        go e1 e2 = op e2 e1 (formatTopProductExpr m (e2 {productExprConst = abs (productExprConst e2)}))
         k' e'
           | sumExprConst e > 0 = Plus' e' (LitInt' (sumExprConst e))
           | sumExprConst e < 0 = Minus' e' (LitInt' (abs (sumExprConst e)))
           | otherwise = e'
-     in k' (foldl go (formatProductExpr eHead) esTail)
+     in k' (foldl go (formatTopProductExpr m eHead) esTail)
 
 -- | `formatTopModuloExpr` convert `ModuloExpr` to `Expr` with adding `FloorMod` at only the root.
 formatTopModuloExpr :: ModuloExpr -> Expr
-formatTopModuloExpr e = FloorMod' (formatSumExpr . unModuloExpr $ normalizeModuloExpr e) (unModulo $ modulo e)
+formatTopModuloExpr e = FloorMod' (formatTopSumExpr (modulo e) . unModuloExpr $ normalizeModuloExpr e) (unModulo $ modulo e)
+
+formatBottomInteger :: Modulo -> Integer -> Expr
+formatBottomInteger m k = case unModulo m of
+  LitInt' m -> LitInt' (k `mod` m)
+  m -> FloorMod' (LitInt' k) m
+
+formatBottomProductExpr :: Modulo -> ProductExpr -> Expr
+formatBottomProductExpr m e =
+  let k = formatBottomInteger m (productExprConst e)
+      k' e' = case productExprConst e of
+        0 -> LitInt' 0
+        1 -> e'
+        -1 -> ModNegate' e' (unModulo m)
+        _ -> ModMult' e' k (unModulo m)
+      invList = map (`ModInv'` unModulo m) (productExprInvList e)
+      list = map (\e -> if e `isModulo` m then e else FloorMod' e (unModulo m)) (productExprList e)
+   in case list ++ invList of
+        [] -> k
+        eHead : esTail -> k' (foldl (\e1 e2 -> ModMult' e1 e2 (unModulo m)) eHead esTail)
+
+formatBottomSumExpr :: Modulo -> SumExpr -> Expr
+formatBottomSumExpr m e = case sumExprList e of
+  [] -> formatBottomInteger m (sumExprConst e)
+  eHead : esTail ->
+    let go e1 e2
+          | productExprConst e2 == 0 = e1
+          | productExprConst e2 == -1 = ModMinus' e1 (formatBottomProductExpr m (e2 {productExprConst = 1})) (unModulo m)
+          | otherwise = ModPlus' e1 (formatBottomProductExpr m e2) (unModulo m)
+        plusConst e'
+          | sumExprConst e == 0 = e'
+          | otherwise = ModPlus' e' (formatBottomInteger m (sumExprConst e)) (unModulo m)
+     in plusConst (foldl go (formatBottomProductExpr m eHead) esTail)
 
 -- | `formatBottomModuloExpr` convert `ModuloExpr` to `Expr` with adding `FloorMod` at every nodes.
 formatBottomModuloExpr :: ModuloExpr -> Expr
-formatBottomModuloExpr = error "Jikka.Core.Language.ModuloExpr.formatBottomModuloExpr: TODO: implement this"
+formatBottomModuloExpr e = formatBottomSumExpr (modulo e) . unModuloExpr $ normalizeModuloExpr e
 
 normalizeProductExpr :: Modulo -> ProductExpr -> ProductExpr
 normalizeProductExpr m e =
@@ -223,18 +332,33 @@ normalizeProductExpr m e =
         if k == 0
           then []
           else sort (productExprList e)
+      es' =
+        if k == 0
+          then []
+          else sort (productExprInvList e)
    in e
         { productExprList = es,
+          productExprInvList = es',
           productExprConst = k
         }
 
 normalizeSumExpr :: Modulo -> SumExpr -> SumExpr
 normalizeSumExpr m e =
-  let cmp e1 e2 = productExprList e1 `compare` productExprList e2
+  let f e = (productExprList e, productExprInvList e)
+      cmp e1 e2 = f e1 `compare` f e2
       cmp' e1 e2 = cmp e1 e2 == EQ
       es = sortBy cmp (map (normalizeProductExpr m) (sumExprList e))
       es' = groupBy cmp' es
-      es'' = map (\group -> ProductExpr {productExprConst = sum (map productExprConst group), productExprList = productExprList (head group)}) es'
+      es'' =
+        map
+          ( \group ->
+              ProductExpr
+                { productExprConst = sum (map productExprConst group),
+                  productExprList = productExprList (head group),
+                  productExprInvList = productExprInvList (head group)
+                }
+          )
+          es'
       es''' = filter (\e -> productExprConst e /= 0 && not (null (productExprList e))) es''
       k = sum (map (\e -> if null (productExprList e) then productExprConst e else 0) es'')
       k' = sumExprConst e + k
@@ -259,4 +383,4 @@ moduloOfModuloExpr :: ModuloExpr -> Expr
 moduloOfModuloExpr = unModulo . modulo
 
 arithmeticExprFromModuloExpr :: ModuloExpr -> ArithmeticExpr
-arithmeticExprFromModuloExpr = parseArithmeticExpr . formatSumExpr . unModuloExpr
+arithmeticExprFromModuloExpr e = parseArithmeticExpr . formatTopSumExpr (modulo e) $ unModuloExpr e
