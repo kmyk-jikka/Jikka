dc049045632a56b595cc8ec4264f43a60a53cab5
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-08-25 00:18:38 +0900

    fix(python): Allow programs without trailing newlines

--- test/Jikka/Python/Parse/AlexSpec.hs
+++ test/Jikka/Python/Parse/AlexSpec.hs
@@ -20,17 +20,17 @@ spec :: Spec
 spec = describe "run" $ do
   it "works" $ do
     let input = "abc ** 123"
-    let tokens = [Ident "abc", PowOp, Int 123]
+    let tokens = [Ident "abc", PowOp, Int 123, Newline]
     run' input `shouldBe` Right tokens
   it "puts 1-based position info" $ do
     let input = "abc def\n123"
-    let tokens = [WithLoc (Loc 1 1 3) $ Ident "abc", WithLoc (Loc 1 5 3) Def, WithLoc (Loc 1 8 1) Newline, WithLoc (Loc 2 1 3) $ Int 123]
+    let tokens = [WithLoc (Loc 1 1 3) $ Ident "abc", WithLoc (Loc 1 5 3) Def, WithLoc (Loc 1 8 1) Newline, WithLoc (Loc 2 1 3) $ Int 123, WithLoc (Loc 0 0 0) Newline]
     run input `shouldBe` Right tokens
   it "inserts <indent> tokens" $ do
     let input = "if:\n    return"
-    let tokens = [If, Colon, Newline, Indent, Return, Dedent]
+    let tokens = [If, Colon, Newline, Indent, Return, Newline, Dedent]
     run' input `shouldBe` Right tokens
   it "uses the longest match" $ do
     let input = "i in int ints"
-    let tokens = [Ident "i", In, Ident "int", Ident "ints"]
+    let tokens = [Ident "i", In, Ident "int", Ident "ints", Newline]
     run' input `shouldBe` Right tokens
--- test/Jikka/Python/ParseSpec.hs
+++ test/Jikka/Python/ParseSpec.hs
@@ -16,15 +16,24 @@ import Test.Hspec
 at :: a -> (Int, Int, Int) -> WithLoc a
 at a (y, x, width) = WithLoc (Loc y x width) a
 
-run' :: [String] -> Either Error Program
-run' lines = run "test.py" (pack $ unlines lines)
+run' :: String -> Either Error Program
+run' prog = run "test.py" (pack prog)
 
 spec :: Spec
 spec = describe "run" $ do
   it "works" $ do
     let input =
-          [ "def solve() -> int:",
-            "    return 42"
-          ]
+          unlines
+            [ "def solve() -> int:",
+              "    return 42"
+            ]
+    let parsed = [FunctionDef ("solve" `at` (1, 5, 5)) emptyArguments [Return (Just (constIntExp 42 `at` (2, 12, 2))) `at` (2, 5, 6)] [] (Just (Name ("int" `at` (1, 16, 3)) `at` (1, 16, 3))) `at` (1, 1, 3)]
+    run' input `shouldBe` Right parsed
+  it "works even without trailing newline" $ do
+    let input = "def solve() -> int:\n    return 42"
+    let parsed = [FunctionDef ("solve" `at` (1, 5, 5)) emptyArguments [Return (Just (constIntExp 42 `at` (2, 12, 2))) `at` (2, 5, 6)] [] (Just (Name ("int" `at` (1, 16, 3)) `at` (1, 16, 3))) `at` (1, 1, 3)]
+    run' input `shouldBe` Right parsed
+  it "works even with CRLF" $ do
+    let input = "def solve() -> int:\r\n    return 42\r\n"
     let parsed = [FunctionDef ("solve" `at` (1, 5, 5)) emptyArguments [Return (Just (constIntExp 42 `at` (2, 12, 2))) `at` (2, 5, 6)] [] (Just (Name ("int" `at` (1, 16, 3)) `at` (1, 16, 3))) `at` (1, 1, 3)]
     run' input `shouldBe` Right parsed
