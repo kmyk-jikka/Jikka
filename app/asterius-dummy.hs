c547e8cb2174da67f9ad632020070128687c3986
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-09-02 00:29:24 +0900

    chore: Delete an unused file app/asterius-dummy.hs

--- app/asterius-dummy.hs
+++
@@ -1,2 +0,0 @@
-main :: IO ()
-main = putStrLn "Please rebuild with compile-web-assembly flag in the Asterius container. e.g. $ docker run -it --rm -v $(pwd):/workspace -w /workspace terrorjack/asterius cabal new-build --flag compile-web-assembly"
