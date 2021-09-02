aa3402662335a09afc16a38eea20d3dc6af48814
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-09-02 19:46:44 +0900

    test: Use sum-sum-square for testing

---
+++ examples/sum_sum_square.py
@@ -0,0 +1,22 @@
+# https://judge.kimiyuki.net/problem/sum-sum-square
+from typing import *
+
+
+def solve(a: List[int]) -> int:
+    ans = 0
+    for a_i in a:
+        for a_j in a:
+            ans += (a_i - a_j) ** 2
+    return ans % 998244353
+
+
+def main() -> None:
+    n = int(input())
+    a = list(map(int, input().split()))
+    assert len(a) == n
+    ans = solve(a)
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
