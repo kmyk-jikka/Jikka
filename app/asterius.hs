fab0cbe8bb2a1c575d742e8e0d261c4c17c7ee16
Kimiyuki Onaka <kimiyuki95@gmail.com>
2021-09-04 13:33:14 +0900

    docs: Add a checkbox "Embed Original Code" to the playground

--- app/asterius.hs
+++ app/asterius.hs
@@ -4,6 +4,7 @@ import Asterius.Types
 import Control.Monad.Except
 import qualified Data.Text as T
 import qualified Jikka.CPlusPlus.Convert.BundleRuntime as BundleRuntime
+import qualified Jikka.CPlusPlus.Convert.EmbedOriginalCode as EmbedOriginalCode
 import Jikka.Common.Format.Error
 import qualified Jikka.Main.Subcommand.Convert as Convert
 import Jikka.Main.Target
@@ -27,6 +28,14 @@ bundleRuntime :: JSString -> JSString
 bundleRuntime = toJSString . bundleRuntime' . fromJSString
 
 foreign export javascript "bundleRuntime" bundleRuntime :: JSString -> JSString
+
+embedOriginalCode' :: String -> String -> String
+embedOriginalCode' input output = T.unpack (EmbedOriginalCode.run (T.pack input) (T.pack output))
+
+embedOriginalCode :: JSString -> JSString -> JSString
+embedOriginalCode input output = toJSString (embedOriginalCode' (fromJSString input) (fromJSString output))
+
+foreign export javascript "embedOriginalCode" embedOriginalCode :: JSString -> JSString -> JSString
 
 main :: IO ()
 main = return ()
